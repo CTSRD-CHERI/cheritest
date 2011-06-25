b4dfbe5e21ce9d379618e2f2e86ec38a1b7523bc
rnw24 <rnw24@cl.cam.ac.uk>
2011-06-25 12:01:41 +0000

    Add CHERI unit tests for bgtzl (branch on greater than zero, likely) based on similar tests for bgtz.
    Two tests fail for the same reason one test fails in btgz: the instruction
    improperly branches when passed a negative argument, causing the branch
    delay slot to be executed and another instruction improperly skipped.

---
+++ tests/test_raw_bgtzl_gt.py
@@ -0,0 +1,14 @@
+from bsim_utils import BaseBsimTestCase
+
+class raw_bgtzl_gt(BaseBsimTestCase):
+    def test_before_bgtzl(self):
+        self.assertRegisterEqual(self.MIPS.a0, 1, "instruction before forward bgtzl missed")
+
+    def test_bgtzl_branch_delay(self):
+        self.assertRegisterEqual(self.MIPS.a1, 2, "instruction in branch-delay slot missed")
+
+    def test_bgtzl_skipped(self):
+        self.assertRegisterNotEqual(self.MIPS.a2, 3, "branch didn't happen")
+
+    def test_bgtzl_target(self):
+        self.assertRegisterEqual(self.MIPS.a3, 4, "instruction at branch target didn't run")
