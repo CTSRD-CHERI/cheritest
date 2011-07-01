4bfaabf61f0547ca8a1bc034dec6fb7793a9d90c
jdw57 <jdw57@cl.cam.ac.uk>
2011-07-01 10:06:18 +0000

    Fixed raw_blezl_gt test which used blez (no l) by mistake.

--- tests/raw_blezl_gt.s
+++ tests/raw_blezl_gt.s
@@ -10,7 +10,7 @@
 start:
 		li	$a0, 1		# before
 		li	$t0, 1
-		blez	$t0, branch_target
+		blezl	$t0, branch_target
 		li	$a1, 2		# branch-delay slot; shouldn't run
 		li	$a2, 3		# should run
 branch_target:
