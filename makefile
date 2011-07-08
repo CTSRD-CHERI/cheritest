7205574da51204a49f1edd55365c1b5fbec40f11
rnw24 <rnw24@cl.cam.ac.uk>
2011-07-08 09:25:21 +0000

    Implement tests for the CP0 "lladdr" register, which tracks the current watched address for the purposes of load linked and store conditional.
    Currently, most tests fail; this means the test isn't very well tested,
    and once lladdr is implemented they may, themselves, require further
    debugging.

--- makefile
+++ makefile
@@ -229,7 +229,8 @@ TEST_FILES=					\
 		test_tnei_lt.s			\
 		test_llsc.s			\
 		test_lldscd.s			\
-		test_cp0_compare.s
+		test_cp0_compare.s		\
+		test_cp0_lladdr.s
 
 #
 # We unconditionally terminate the simulator after TEST_CYCLE_LIMIT
---
+++ tests/test_cp0_lladdr.s
@@ -0,0 +1,120 @@
+.set mips64
+.set noreorder
+.set nobopt
+.set noat
+
+#
+# Check that the CP0 "lladdr" register is properly updated during load linked
+# and store conditional operations.
+#
+
+		.global test
+test:		.ent test
+		daddu 	$sp, $sp, -32
+		sd	$ra, 24($sp)
+		sd	$fp, 16($sp)
+		daddu	$fp, $sp, 32
+
+		#
+		# Save addresses that will be checked in test results
+		#
+		dla	$s0, word1
+		dla	$s1, word2
+		dla	$s2, word3
+		dla	$s3, dword1
+		dla	$s4, dword2
+		dla	$s5, dword3
+
+		#
+		# Query value on reset
+		#
+		dmfc0	$a0, $17
+
+		#
+		# Simple load linked and store conditional
+		#
+		ll	$t0, word1
+		nop
+		nop
+		nop
+		dmfc0	$a1, $17
+		sc	$t0, word1
+		nop
+		nop
+		nop
+		dmfc0	$a2, $17
+
+		#
+		# Simple load linked and store conditional double word1
+		#
+		lld	$t0, dword1
+		nop
+		nop
+		nop
+		dmfc0	$a3, $17
+		scd	$t0, dword1
+		nop
+		nop
+		nop
+		dmfc0	$a4, $17
+
+		#
+		# If we do two load linkeds in a row, we get the second one.
+		#
+		ll	$t0, word1
+		ll	$t0, word2
+		nop
+		nop
+		nop
+		dmfc0	$a5, $17
+
+		#
+		# If we do two load linked double words in a row, we get the
+		# second one.
+		#
+		lld	$t0, dword1
+		lld	$t0, dword2
+		nop
+		nop
+		nop
+		dmfc0	$a6, $17
+
+		#
+		# If we interrupt the load linked, store conditional through
+		# an operation that clears LLbit, we should still get the
+		# same address.
+		#
+		ll	$t0, word3
+		sw	$zero, word3
+		nop
+		nop
+		nop
+		dmfc0	$a7, $17
+
+		#
+		# If we interrupt the load linked, store conditional double
+		# word through an operation that clears LLbit, we should
+		# still get the same address.
+		#
+		lld	$t0, dword3
+		sd	$zero, dword3
+		nop
+		nop
+		nop
+		dmfc0	$s6, $17
+
+		ld	$fp, 16($sp)
+		ld	$ra, 24($sp)
+		daddu	$sp, $sp, 32
+		jr	$ra
+		nop			# branch-delay slot
+		.end	test
+
+		.data
+word1:		.word	0xffffffff
+word2:		.word	0xffffffff
+word3:		.word	0xffffffff
+fill:		.word	0x00000000		
+dword1:		.dword	0xffffffffffffffff
+dword2:		.dword	0xffffffffffffffff
+dword3:		.dword	0xffffffffffffffff
