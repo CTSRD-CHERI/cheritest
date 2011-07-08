a440c8e17a47c0d64cf5b64fe083ae11538914ee
jdw57 <jdw57@cl.cam.ac.uk>
2011-07-08 14:06:13 +0000

    Added final divide result validation to the mul_div_loop test from my compiled tests.

--- tests/test_mul_div_loop.py
+++ tests/test_mul_div_loop.py
@@ -4,5 +4,6 @@ class raw_mul_div_loop(BaseBsimTestCase):
     def test_a0(self):
         '''Test that result of multiply and divide loop test is correct.'''
         self.assertRegisterEqual(self.MIPS.s7, 0x0000000000058980, "Final Multiply Result is Incorrect")
+		self.assertRegisterEqual(self.MIPS.s7, 0x0000000000009D80, "Final Divide Result is Incorrect")
 
 		
