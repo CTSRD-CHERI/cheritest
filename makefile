819fed444b52a9e2c812687c6e1d1e74e4c94e81
rnw24 <rnw24@cl.cam.ac.uk>
2011-10-23 18:47:08 +0000

    Add a CHERI unit test implementing a complete capability register context switch, writing out 30 registers and reading them back using cscr and clcr.

--- makefile
+++ makefile
@@ -297,6 +297,7 @@ TEST_CP2_FILES=					\
 		test_cp2_cmove.s		\
 		test_cp2_cscr.s			\
 		test_cp2_clcr.s			\
+		test_cp2_cscr_clcr_context.s	\
 		test_cp2_alu_mod_pipeline.s	\
 		test_cp2_mem_mod_pipeline.s	\
 		test_cp2_get_alu_pipeline.s	\
---
+++ tests/cp2/test_cp2_cscr_clcr_context.s
@@ -0,0 +1,244 @@
+#-
+# Copyright (c) 2011 Robert N. M. Watson
+# All rights reserved.
+#
+# This software was developed by SRI International and the University of
+# Cambridge Computer Laboratory under DARPA/AFRL contract (FA8750-10-C-0237)
+# ("CTSRD"), as part of the DARPA CRASH research programme.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+# OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+# OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
+#
+
+.set mips64
+.set noreorder
+.set nobopt
+.set noat
+
+#
+# Macro test that checks whether we can save and restore the complete
+# capability register file.  All loads and stores are performed via $c30
+# ($kdc).  This replicates the work an operating system would perform in
+# order to ensure that an OS would work!
+#
+
+		.global test
+test:		.ent test
+		daddu	$sp, $sp, -32
+		sd	$ra, 24($sp)
+		sd	$fp, 16($sp)
+		daddu	$fp, $sp, 32
+
+		#
+		# Save out all capability registers but $kcc and $kdc.
+		#
+		dla	$t0, data
+		cscr	$c0, $c30, $t0
+
+		daddiu	$t0, $t0, 32
+		cscr	$c1, $c30, $t0
+
+		daddiu	$t0, $t0, 32
+		cscr	$c2, $c30, $t0
+
+		daddiu	$t0, $t0, 32
+		cscr	$c3, $c30, $t0
+
+		daddiu	$t0, $t0, 32
+		cscr	$c4, $c30, $t0
+
+		daddiu	$t0, $t0, 32
+		cscr	$c5, $c30, $t0
+
+		daddiu	$t0, $t0, 32
+		cscr	$c6, $c30, $t0
+
+		daddiu	$t0, $t0, 32
+		cscr	$c7, $c30, $t0
+
+		daddiu	$t0, $t0, 32
+		cscr	$c8, $c30, $t0
+
+		daddiu	$t0, $t0, 32
+		cscr	$c9, $c30, $t0
+
+		daddiu	$t0, $t0, 32
+		cscr	$c10, $c30, $t0
+
+		daddiu	$t0, $t0, 32
+		cscr	$c11, $c30, $t0
+
+		daddiu	$t0, $t0, 32
+		cscr	$c12, $c30, $t0
+
+		daddiu	$t0, $t0, 32
+		cscr	$c13, $c30, $t0
+
+		daddiu	$t0, $t0, 32
+		cscr	$c14, $c30, $t0
+
+		daddiu	$t0, $t0, 32
+		cscr	$c15, $c30, $t0
+
+		daddiu	$t0, $t0, 32
+		cscr	$c16, $c30, $t0
+
+		daddiu	$t0, $t0, 32
+		cscr	$c17, $c30, $t0
+
+		daddiu	$t0, $t0, 32
+		cscr	$c18, $c30, $t0
+
+		daddiu	$t0, $t0, 32
+		cscr	$c19, $c30, $t0
+
+		daddiu	$t0, $t0, 32
+		cscr	$c20, $c30, $t0
+
+		daddiu	$t0, $t0, 32
+		cscr	$c21, $c30, $t0
+
+		daddiu	$t0, $t0, 32
+		cscr	$c22, $c30, $t0
+
+		daddiu	$t0, $t0, 32
+		cscr	$c23, $c30, $t0
+
+		daddiu	$t0, $t0, 32
+		cscr	$c24, $c30, $t0
+
+		daddiu	$t0, $t0, 32
+		cscr	$c25, $c30, $t0
+
+		daddiu	$t0, $t0, 32
+		cscr	$c26, $c30, $t0
+
+		daddiu	$t0, $t0, 32
+		cscr	$c27, $c30, $t0
+
+		daddiu	$t0, $t0, 32
+		cscr	$c28, $c30, $t0
+
+		daddiu	$t0, $t0, 32
+		cscr	$c31, $c30, $t0
+
+		#
+		# Now reverse the process.
+		#
+		dla	$t0, data
+		clcr	$c0, $c30, $t0
+
+		daddiu	$t0, $t0, 32
+		clcr	$c1, $c30, $t0
+
+		daddiu	$t0, $t0, 32
+		clcr	$c2, $c30, $t0
+
+		daddiu	$t0, $t0, 32
+		clcr	$c3, $c30, $t0
+
+		daddiu	$t0, $t0, 32
+		clcr	$c4, $c30, $t0
+
+		daddiu	$t0, $t0, 32
+		clcr	$c5, $c30, $t0
+
+		daddiu	$t0, $t0, 32
+		clcr	$c6, $c30, $t0
+
+		daddiu	$t0, $t0, 32
+		clcr	$c7, $c30, $t0
+
+		daddiu	$t0, $t0, 32
+		clcr	$c8, $c30, $t0
+
+		daddiu	$t0, $t0, 32
+		clcr	$c9, $c30, $t0
+
+		daddiu	$t0, $t0, 32
+		clcr	$c10, $c30, $t0
+
+		daddiu	$t0, $t0, 32
+		clcr	$c11, $c30, $t0
+
+		daddiu	$t0, $t0, 32
+		clcr	$c12, $c30, $t0
+
+		daddiu	$t0, $t0, 32
+		clcr	$c13, $c30, $t0
+
+		daddiu	$t0, $t0, 32
+		clcr	$c14, $c30, $t0
+
+		daddiu	$t0, $t0, 32
+		clcr	$c15, $c30, $t0
+
+		daddiu	$t0, $t0, 32
+		clcr	$c16, $c30, $t0
+
+		daddiu	$t0, $t0, 32
+		clcr	$c17, $c30, $t0
+
+		daddiu	$t0, $t0, 32
+		clcr	$c18, $c30, $t0
+
+		daddiu	$t0, $t0, 32
+		clcr	$c19, $c30, $t0
+
+		daddiu	$t0, $t0, 32
+		clcr	$c20, $c30, $t0
+
+		daddiu	$t0, $t0, 32
+		clcr	$c21, $c30, $t0
+
+		daddiu	$t0, $t0, 32
+		clcr	$c22, $c30, $t0
+
+		daddiu	$t0, $t0, 32
+		clcr	$c23, $c30, $t0
+
+		daddiu	$t0, $t0, 32
+		clcr	$c24, $c30, $t0
+
+		daddiu	$t0, $t0, 32
+		clcr	$c25, $c30, $t0
+
+		daddiu	$t0, $t0, 32
+		clcr	$c26, $c30, $t0
+
+		daddiu	$t0, $t0, 32
+		clcr	$c27, $c30, $t0
+
+		daddiu	$t0, $t0, 32
+		clcr	$c28, $c30, $t0
+
+		daddiu	$t0, $t0, 32
+		clcr	$c31, $c30, $t0
+
+		.end test
+
+		#
+		# 32-byte aligned storage for 30 adjacent capability
+		# registers.
+		#
+		.data
+		.align 5
+data:		.fill 960
