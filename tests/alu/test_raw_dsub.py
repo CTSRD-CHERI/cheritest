ee7e1b684f5c767d25fb67ed0a8c8f6f9cb2bf00
rmn30 <rmn30@cl.cam.ac.uk>
2012-01-06 15:49:19 +0000

    fix major copy-and-paste error which meant test_raw_dsub test was actually the same as test_raw_dadd

--- tests/alu/test_raw_dsub.py
+++ tests/alu/test_raw_dsub.py
@@ -29,33 +29,33 @@
 #
 from cheritest_tools import BaseCHERITestCase
 
-class test_raw_dadd(BaseCHERITestCase):
+class test_raw_dsub(BaseCHERITestCase):
     def test_independent_inputs(self):
         '''Check that simple dadd worked, no input modification'''
-        self.assertRegisterEqual(self.MIPS.s3, 1, "dadd modified first input")
-        self.assertRegisterEqual(self.MIPS.s4, 2, "dadd modified second input")
-        self.assertRegisterEqual(self.MIPS.a0, 3, "dadd failed")
+        self.assertRegisterEqual(self.MIPS.s3, 2, "dsub modified first input")
+        self.assertRegisterEqual(self.MIPS.s4, 1, "dsub modified second input")
+        self.assertRegisterEqual(self.MIPS.a0, 1, "dsub failed")
 
     def test_into_first_input(self):
-        self.assertRegisterEqual(self.MIPS.a1, 3, "dadd into first input failed")
+        self.assertRegisterEqual(self.MIPS.a1, 1, "dsub into first input failed")
 
     def test_into_second_input(self):
-        self.assertRegisterEqual(self.MIPS.a2, 3, "dadd into second input failed")
+        self.assertRegisterEqual(self.MIPS.a2, 1, "dsub into second input failed")
 
     def test_into_both_input(self):
-        self.assertRegisterEqual(self.MIPS.a3, 2, "dadd into both inputs failed")
+        self.assertRegisterEqual(self.MIPS.a3, 0, "dsub into both inputs failed")
 
     def test_pipeline(self):
-        self.assertRegisterEqual(self.MIPS.a4, 6, "dadd-to-dadd pipeline failed")
+        self.assertRegisterEqual(self.MIPS.a4, 1, "dsub-to-dsub pipeline failed")
 
     def test_pos_neg_to_zero(self):
-        self.assertRegisterEqual(self.MIPS.a5, 0, "positive plus negative to zero failed")
+        self.assertRegisterEqual(self.MIPS.a5, 0, "positive minus positive to zero failed")
 
     def test_neg_neg_to_neg(self):
-        self.assertRegisterEqual(self.MIPS.a6, 0xfffffffffffffffe, "negative plus negative to negative failed")
+        self.assertRegisterEqual(self.MIPS.a6, 0xfffffffffffffffe, "negative minus negative to negative failed")
 
     def test_neg_pos_to_pos(self):
-        self.assertRegisterEqual(self.MIPS.a7, 1, "negative plus positive to positive failed")
+        self.assertRegisterEqual(self.MIPS.a7, 1, "negative minus negative to positive failed")
 
     def test_neg_pos_to_neg(self):
-        self.assertRegisterEqual(self.MIPS.s0, 0xffffffffffffffff, "positive plus negative to negative failed")
+        self.assertRegisterEqual(self.MIPS.s0, 0xffffffffffffffff, "positive minus positive to negative failed")
