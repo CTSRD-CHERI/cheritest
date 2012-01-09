ac7f431b157624f85932446b534eb623d0492589
rmn30 <rmn30@cl.cam.ac.uk>
2012-01-09 16:03:23 +0000

    fix load left test which was broken in at least two ways: 1) read from unitialised memory with offset 8 2) doesn't really check that part of destination register is preserved due to being initialised to zero. other load left/right tests need similarly updating(to follow).

--- tests/mem/test_raw_ldl.py
+++ tests/mem/test_raw_ldl.py
@@ -34,25 +34,25 @@ class test_raw_ldl(BaseCHERITestCase):
 		self.assertRegisterEqual(self.MIPS.a1, 0xfedcba9876543210, "LDL with zero offset failed")
 
 	def test_offset_one(self):
-		self.assertRegisterEqual(self.MIPS.a2, 0xdcba987654321000, "LDL with one offset failed")
+		self.assertRegisterEqual(self.MIPS.a2, 0xdcba9876543210b0, "LDL with one offset failed")
 
 	def test_offset_two(self):
-		self.assertRegisterEqual(self.MIPS.a3, 0xba98765432100000, "LDL with two offset failed")
+		self.assertRegisterEqual(self.MIPS.a3, 0xba9876543210b1b0, "LDL with two offset failed")
 
 	def test_offset_three(self):
-		self.assertRegisterEqual(self.MIPS.a4, 0x9876543210000000, "LDL with three offset failed")
+		self.assertRegisterEqual(self.MIPS.a4, 0x9876543210b2b1b0, "LDL with three offset failed")
 
 	def test_offset_four(self):
-		self.assertRegisterEqual(self.MIPS.a5, 0x7654321000000000, "LDL with four offset failed")
+		self.assertRegisterEqual(self.MIPS.a5, 0x76543210b3b2b1b0, "LDL with four offset failed")
 
 	def test_offset_five(self):
-		self.assertRegisterEqual(self.MIPS.a6, 0x5432100000000000, "LDL with five offset failed")
+		self.assertRegisterEqual(self.MIPS.a6, 0x543210b4b3b2b1b0, "LDL with five offset failed")
 
 	def test_offset_six(self):
-		self.assertRegisterEqual(self.MIPS.a7, 0x3210000000000000, "LDL with six offset failed")
+		self.assertRegisterEqual(self.MIPS.a7, 0x3210b5b4b3b2b1b0, "LDL with six offset failed")
 
 	def test_offset_seven(self):
-		self.assertRegisterEqual(self.MIPS.t0, 0x1000000000000000, "LDL with seven offset failed")
+		self.assertRegisterEqual(self.MIPS.t0, 0x10b6b5b4b3b2b1b0, "LDL with seven offset failed")
 
 	def test_offset_eight(self):
-		self.assertRegisterEqual(self.MIPS.t1, 0x0000000000000000, "LDL with eight offset (skip double word) failed")
+		self.assertRegisterEqual(self.MIPS.t1, 0xffffffffffffffff, "LDL with eight offset (skip double word) failed")
