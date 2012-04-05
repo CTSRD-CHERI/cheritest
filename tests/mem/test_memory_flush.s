c69a27904306494388a8ec245fc5fa6fcc4e67aa
jdw57 <jdw57@cl.cam.ac.uk>
2012-04-05 17:13:12 +0000

    Added interrupted store test which tests that a store upon which an interrupt fires does not make it out to memory.

---
+++ tests/mem/test_memory_flush.s
@@ -0,0 +1,115 @@
+#-
+# Copyright (c) 2011 Robert N. M. Watson
+# All rights reserved.
+#
+# This software was developed by SRI International and the University of
+# Cambridge Computer Laboratory under DARPA/AFRL contract (FA8750-10-C-0237)
+# ("CTSRD"), as part of the DARPA CRASH research programme.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+# OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+# OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
+#
+
+.set mips64
+.set noreorder
+.set nobopt
+.set noat
+
+#
+# Exercise trap instruction 'tne' (trap on not equal), less than case.
+#
+
+		.global test
+test:		.ent test
+		daddu 	$sp, $sp, -32
+		sd	$ra, 24($sp)
+		sd	$fp, 16($sp)
+		daddu	$fp, $sp, 32
+
+		#
+		# Set up exception handler.
+		#
+		jal	bev_clear
+		nop
+		dla	$a0, bev0_handler
+		jal	bev0_handler_install
+		nop
+
+		#
+		# Clear registers we'll use when testing results later.
+		#
+		dli	$a0, 0
+		dli	$a1, 0
+		dli	$a2, 0
+		dli	$a3, 0
+		dli	$a4, 0
+		dli	$a5, 0
+		dli	$a6, 0
+		mfc0	$t0, $12
+		ori	$t0, 0x3e1
+		mtc0	$t0, $12	# Enable software interrupts
+		nop
+		nop
+		nop
+		nop
+		nop
+
+		dli	$a0, 0x9800000000001000 # Address to store things
+		sd	$0,  0($a0)		# Initialize the location to 0
+		dli	$t0, 0x9000000000000000 # Load an uncached address to force cache misses.
+		ld	$t1, 0($t0)
+		nop
+		nop
+		nop
+		nop
+		li	$t0, 0x100
+		mtc0	$t0, $13
+		sd	$a0, 0($a0)
+		ld	$a1, 0($a0)
+
+return:
+		ld	$fp, 16($sp)
+		ld	$ra, 24($sp)
+		daddu	$sp, $sp, 32
+		jr	$ra
+		nop			# branch-delay slot
+		.end	test
+
+#
+# Our actual exception handler, which tests various properties.  This code
+# assumes that the trap wasn't in a branch-delay slot (and the test code
+# checks BD as well), so EPC += 4 should return control after the victim.
+#
+		.ent bev0_handler
+bev0_handler:	
+		dli	$k0, 0xFFFF00FF
+		dmfc0	$a5, $13	# Cause register
+		and	$k0, $a5, $k0	# # Clear interrupts in the cause register
+		dmtc0	$k0, $13
+		dmfc0	$a5, $14	# EPC
+		daddiu	$k0, $a5, 4	# EPC += 4 to bump PC forward on ERET to skip the victim
+		dmtc0	$k0, $14
+		nop			# NOPs to avoid hazard with ERET
+		nop			# XXXRW: How many are actually
+		nop			# required here?
+		nop
+		eret
+		.end bev0_handler
