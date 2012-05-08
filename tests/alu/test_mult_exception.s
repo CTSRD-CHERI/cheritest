316299c52519d40dcdf7fda33c93de255a7acf2b
jdw57 <jdw57@cl.cam.ac.uk>
2012-05-08 15:43:37 +0000

    Added copyright name.  Mine.

--- tests/alu/test_mult_exception.s
+++ tests/alu/test_mult_exception.s
@@ -1,5 +1,6 @@
 #-
 # Copyright (c) 2011 Robert N. M. Watson
+#               2012 Jonathan D. Woodruff
 # All rights reserved.
 #
 # This software was developed by SRI International and the University of
