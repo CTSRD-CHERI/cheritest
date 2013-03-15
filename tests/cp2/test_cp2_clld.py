32235f3e86a01c3eea5e62bf889c97ec13c7a495
mr101 <mr101@cl.cam.ac.uk>
2013-03-15 19:45:14 +0000

    Added a test for capability load linked doubleword and capability store conditional doubleword.
    This test fails at the moment:
    
    - The installed version of the assembler doesn't support clldr
    
    - There is a mismatch between the ISA description in the Architecture
      document and current assembler behaviour over the sense of the s bit
      in the load/store via capability instructions.
    
    - The CHERI-1 implementation of cscd appears to not set rd according to
      whether the store succeeded or failed; but this may be due to the s bit
      problem (the CPU doesn't recognise the opcode as a cscd?)

---
+++ tests/cp2/test_cp2_clld.py
@@ -0,0 +1,73 @@
+#-
+# Copyright (c) 2011 Robert N. M. Watson
+# All rights reserved.
+#
+# This software was developed by SRI International and the University of
+# Cambridge Computer Laboratory under DARPA/AFRL contract (FA8750-10-C-0237)
+# ("CTSRD"), as part of the DARPA CRASH research programme.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+# OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+# OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
+#
+from cheritest_tools import BaseCHERITestCase
+from nose.plugins.attrib import attr
+
+class test_cp2_clld(BaseCHERITestCase):
+
+    @attr('llsc')
+    def test_cp2_clld_1(self):
+	'''That an uninterrupted clld+cscd succeeds'''
+        self.assertRegisterEqual(self.MIPS.a0, 1, "Uninterrupted clld+cscd failed")
+
+    @attr('llsc')
+    def test_cp2_clld_2(self):
+	'''That an uninterrupted clld+cscd stored the right value'''
+	self.assertRegisterEqual(self.MIPS.a1, 0xffffffffffffffff, "Uninterrupted clld+cscd stored wrong value")
+
+    @attr('llsc')
+    def test_cp2_clld_3(self):
+	'''That an uninterrupted clld+cld+cscd succeeds'''
+	self.assertRegisterEqual(self.MIPS.a2, 1, "Uninterrupted clld+cld+cscd failed")
+
+    @attr('llsc')
+    def test_cp2_clld_4(self):
+	'''That an uninterrupted clld+add+cscd succeeds'''
+	self.assertRegisterEqual(self.MIPS.a3, 1, "Uninterrupted clld+add+cscd failed")
+
+    @attr('llsc')
+    def test_cp2_clld_5(self):
+	'''That an uninterrupted clld+add+cscd stored the right value'''
+	self.assertRegisterEqual(self.MIPS.a4, 0, "Uninterrupted clld+add+cscd stored wrong value")
+
+    @attr('llsc')
+    def test_cp2_clld_6(self):
+	'''That an clld+csd+cscd spanning fails'''
+	self.assertRegisterEqual(self.MIPS.t0, 0, "Interrupted clld+csd+cscd succeeded")
+
+    @attr('llsc')
+    def test_cp2_clld_7(self):
+	'''That an clld+cscd spanning a store to the line does not store'''
+	self.assertRegisterNotEqual(self.MIPS.a6, 1, "Interrupted clld+csd+cscd stored value")
+
+    @attr('llsc')
+    def test_cp2_clld_8(self):
+	'''That an clld+cscd spanning a trap fails'''
+	self.assertRegisterEqual(self.MIPS.a7, 0, "Interrupted clld+tnei+cscd succeeded")
---
+++ tests/cp2/test_cp2_clld.s
@@ -0,0 +1,130 @@
+#-
+# Copyright (c) 2011 Robert N. M. Watson, 2013 Michael Roe
+# All rights reserved.
+#
+# This software was developed by SRI International and the University of
+# Cambridge Computer Laboratory under DARPA/AFRL contract (FA8750-10-C-0237)
+# ("CTSRD"), as part of the DARPA CRASH research programme.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+# OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+# OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
+#
+
+.set mips64
+.set noreorder
+.set nobopt
+
+#
+# Check that various operations interrupt the capability versions of
+# load linked + store conditional double word.
+#
+
+		.global test
+test:		.ent test
+		daddu 	$sp, $sp, -32
+		sd	$ra, 24($sp)
+		sd	$fp, 16($sp)
+		daddu	$fp, $sp, 32
+
+		#
+		# Set up nop exception handler.
+		#
+		jal	bev_clear
+		nop
+		dla	$a0, bev0_handler
+		jal	bev0_handler_install
+		nop
+
+		#
+		# Uninterrupted access; check to make sure the right value
+		# comes back.
+		#
+		cmove   $c1, $c0 # FIXME
+		dla	$t1, dword
+		clldr	$a0, $t1($c0)
+		cscdr	$a0, $t1($c0)
+		cldr	$a1, $t1($c0)
+
+		#
+		# Load the double word into another register between clldr and
+		# cscdr; this shouldn't cause the store to fail.
+		#
+		clldr	$a2, $t1($c0)
+		ld	$t0, 0($t1)
+		cscdr	$a2, $t1($c0)
+
+		#
+		# Check to make sure we are allowed to increment the loaded
+		# number, so we can do atomic arithmetic.
+		#
+		clldr	$a3, $t1($c0)
+		addiu	$a3, $a3, 1
+		cscdr	$a3, $t1($c0)
+		ld	$a4, 0($t1)
+
+		#
+		# Store to double word between clldr and cscdr; check to make
+		# sure that the cscdr not only returns failure, but doesn't
+		# store.
+		#
+		li	$t0, 1
+		clldr	$a5, $t1($c0)
+		sd	$a5, 0($t1)
+		cscdr	$t0, $t1($c0)
+		ld	$a6, 0($t1)
+
+		#
+		# Trap between clldr and cscdr; check to make sure that the
+		# cscdr not only returns failure, but doesn't store.
+		# XXX : Where does it check that?
+		#
+		clldr	$a7, $t1($c0)
+		tnei	$zero, 1
+		cscdr	$a7, $t1($c0)
+
+		ld	$fp, 16($sp)
+		ld	$ra, 24($sp)
+		daddu	$sp, $sp, 32
+		jr	$ra
+		nop			# branch-delay slot
+		.end	test
+
+
+#
+# No-op exception handler to return back after the tnei and confirm that the
+# following sc fails.  This code assumes that the trap isn't from a branch-
+# delay slot.
+
+#
+		.ent bev0_handler
+bev0_handler:
+		dmfc0	$k0, $14	# EPC
+		daddiu	$k0, $k0, 4	# EPC += 4 to bump PC forward on ERET
+		dmtc0	$k0, $14
+		nop			# NOPs to avoid hazard with ERET
+		nop			# XXXRW: How many are actually
+		nop			# required here?
+		nop
+		eret
+		.end bev0_handler
+
+		.data
+dword:		.dword	0xffffffffffffffff
