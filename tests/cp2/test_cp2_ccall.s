a05b26542ea526549c1a8bf3504306bdb7877f05
mr101 <mr101@cl.cam.ac.uk>
2013-03-16 18:26:37 +0000

    Added a test for CCall calling the right trap handler.

---
+++ tests/cp2/test_cp2_ccall.s
@@ -0,0 +1,144 @@
+#-
+# Copyright (c) 2012 Michael Roe
+# All rights reserved.
+#
+# This software was developed by SRI International and the University of
+# Cambridge Computer Laboratory under DARPA/AFRL contract (FA8750-10-C-0237)
+# ("CTSRD"), as part of the DARPA CRASH research programme.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+# OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+# OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
+#
+
+.set mips64
+.set noreorder
+.set nobopt
+.set noat
+
+#
+# Test ccall
+#
+
+# In this test, sandbox isn't actually called, but its address is used
+# as an otype.
+sandbox:
+		creturn
+
+		.global test
+test:		.ent test
+		daddu 	$sp, $sp, -32
+		sd	$ra, 24($sp)
+		sd	$fp, 16($sp)
+		daddu	$fp, $sp, 32
+
+		#
+		# Set up exception handler
+		#
+
+		jal	bev_clear
+		nop
+		dla	$a0, bev0_handler
+		jal	bev0_handler_install
+		nop
+
+		#
+		# Set up trap handler for CCall
+		#
+
+		dli	$a0, 0xffffffff80000280
+		dla	$a1, bev0_ccall_handler_stub
+		dli	$a2, 9 		# 32-bit instruction count
+		dsll	$a2, 2		# Convert to byte count
+		jal memcpy
+		nop			# branch-delay slot
+
+                # Make $c1 a template capability for a user-defined type
+		# whose otype is equal to the address of sandbox.
+		dla      $t0, sandbox
+		csettype $c1, $c0, $t0
+
+                # Make $c2 a data capability for the array at address data
+		dla      $t0, data
+		cincbase $c2, $c0, $t0
+                dli      $t0, 8
+                csetlen  $c2, $c2, $t0
+		# Permissions Non_Ephemeral, Permit_Load, Permit_Store,
+		# Permit_Store.
+		# NB: Permit_Execute must not be included in the set of
+		# permissions used here.
+		dli      $t0, 0xd
+		candperm $c2, $c2, $t0
+
+		# Seal data capability $c2 to the otype of $c1, and store
+		# result in $c3.
+                csealdata $c3, $c2, $c1
+
+		# Make $c4 a code capability for sandbox
+		csealcode $c4, $c1
+
+		ccall    $c4, $c3
+		nop
+
+		ld	$fp, 16($sp)
+		ld	$ra, 24($sp)
+		daddu	$sp, $sp, 32
+		jr	$ra
+		nop			# branch-delay slot
+		.end	test
+
+		.ent bev0_handler
+bev0_handler:
+		li	$a2, 1
+		cgetcause $a3
+		dmfc0	$a5, $14	# EPC
+		daddiu	$k0, $a5, 4	# EPC += 4 to bump PC forward on ERET
+		dmtc0	$k0, $14
+		nop
+		nop
+		nop
+		nop
+		eret
+		.end bev0_handler
+
+		.ent bev0_ccall_handler
+bev0_ccall_handler:
+		li      $a2, 0x5
+		cgetcause $a3
+		dmfc0   $a5, $14
+		daddiu  $k0, $a5, 4 # Bump EPC forward one instruction
+		dmtc0   $k0, $14
+		nop
+		nop
+		nop
+		nop
+		eret
+		.end bev0_ccall_handler
+
+		.ent bev0_ccall_handler_stub
+bev0_ccall_handler_stub:
+		dla     $k0, bev0_ccall_handler
+		jr      $k0
+		nop
+		.end bev0_ccall_handler_stub
+
+		.data
+		.align 3
+data:		.dword	0xfedcba9876543210
