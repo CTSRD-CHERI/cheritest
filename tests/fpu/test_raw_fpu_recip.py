7b30c7efe2796221f71015291028822a53f6eb5c
mr101 <mr101@cl.cam.ac.uk>
2013-07-11 17:24:18 +0000

    Tag test_raw_fpu_recip as requiring hardware support for the floating point reciprocal operation.

--- tests/fpu/test_raw_fpu_recip.py
+++ tests/fpu/test_raw_fpu_recip.py
@@ -27,16 +27,20 @@
 # SUCH DAMAGE.
 #
 from cheritest_tools import BaseCHERITestCase
+from nose.plugins.attrib import attr
 
 class test_raw_fpu_recip(BaseCHERITestCase):
+    @attr('floatrecip')
     def test_recip_single(self):
         '''Test we can take reciprocals in single precision'''
         self.assertRegisterEqual(self.MIPS.s1, 0x7F800000, "Failed to take the reciprocal of 0.0 in single precision")
 
+    @attr('floatrecip')
     def test_recip_double(self):
         '''Test we can take reciprocals in double precision'''
         self.assertRegisterEqual(self.MIPS.s0, 0x3FB0000000000000, "Failed to take the reciprocal of 16.0 in double precision")
 
+    @attr('floatrecip')
     def test_recip_edge_cases(self):
         '''Test edge cases of floating point reciprocal'''
         self.assertRegisterEqual(self.MIPS.s3, 0x7FF1000000000000, "Failed to echo QNaN")
