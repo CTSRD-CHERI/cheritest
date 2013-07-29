e760eb090d7e011789d900f6bb5deb9ea49d5608
jdw57 <jdw57@cl.cam.ac.uk>
2013-07-29 13:12:53 +0000

    Jump back to the real ".end" to help gxemul.

--- tests/branch/test_raw_b_maxoffset.s
+++ tests/branch/test_raw_b_maxoffset.s
@@ -59,6 +59,18 @@ start:
 		sw $s2,28($s1)
 		lw $s2,32($s0)
 		sw $s2,32($s1)
+		lw $s2,36($s0)
+		sw $s2,36($s1)
+		lw $s2,40($s0)
+		sw $s2,40($s1)
+		lw $s2,44($s0)
+		sw $s2,44($s1)
+		lw $s2,48($s0)
+		sw $s2,48($s1)
+		lw $s2,52($s0)
+		sw $s2,52($s1)
+		lw $s2,56($s0)
+		sw $s2,56($s1)
 		dli $t0, 0x801fffc     # set the distance we want to move the branch
 		dsub $s1, $s0, $t0     # generate the target address
 		lw $s2, 0($s0)         # move the words to the target
@@ -84,6 +96,9 @@ branch_target:
 
 		# Terminate the simulator
 		mtc0	$v0, $23
+		dla	$t0, end
+		jr	$t0
+		nop
 end:
 		b end
 		nop
