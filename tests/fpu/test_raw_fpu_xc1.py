7098f7d99b2fea26286a4fbf434b92791ab1064c
mr101 <mr101@cl.cam.ac.uk>
2013-07-29 16:10:13 +0000

    Tag test_raw_fpu_xc1 as requiring support for the floating point indexed load and store instructions, which weren't part of MIPS R4000.

--- tests/fpu/test_raw_fpu_xc1.py
+++ tests/fpu/test_raw_fpu_xc1.py
@@ -27,13 +27,16 @@
 # SUCH DAMAGE.
 #
 from cheritest_tools import BaseCHERITestCase
+from nose.plugins.attrib import attr
 
 class test_raw_fpu_xc1(BaseCHERITestCase):
+    @attr('floatindexed')
     def test_words(self):
         '''Test we can store words at indexed location offsets.'''
         self.assertRegisterEqual(self.MIPS.s0, 0xFFFFFFFFABCD0000, 'Failed to save and load offset 0')
         self.assertRegisterEqual(self.MIPS.s1, 0x43210000, 'Failed to save and load offset 4')
 
+    @attr('floatindexed')
     def test_doubles(self):
         '''Test we can store doubles at indexed location offsets.'''
         self.assertRegisterEqual(self.MIPS.s2, 0xFEED000000000000, 'Failed to save and load offset 0.')
