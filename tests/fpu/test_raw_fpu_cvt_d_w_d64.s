cd67302d27ba32a20ea12f3f21659d6f6a1e6a7b
mr101 <mr101@cl.cam.ac.uk>
2013-10-28 13:54:18 +0000

    Added a test for conversion of a 32-bit int to double precision.
    Former-commit-id: 27cfd3bb0062043114246621f3220a64f668b675

---
+++ tests/fpu/test_raw_fpu_cvt_d_w_d64.s
@@ -0,0 +1,80 @@
+#-
+# Copyright (c) 2013 Michael Roe
+# All rights reserved.
+#
+# This software was developed by SRI International and the University of
+# Cambridge Computer Laboratory under DARPA/AFRL contract (FA8750-10-C-0237)
+# ("CTSRD"), as part of the DARPA CRASH research programme.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+# OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+# OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
+#
+.set mips64
+.set noreorder
+.set nobopt
+.set noat
+
+#
+# Test conversion from 32-bit int to double-precision floating point when
+# the FPU is in 64-bit mode
+#
+        .text
+        .global start
+        .ent start
+start:     
+	mfc0 $t0, $12
+        li $t1, 1 << 29		# Enable CP1
+        or $t0, $t0, $t1    
+	li $t1, 1 << 26		# Put FPU into 64 bit mode
+	or $t0, $t0, $t1
+	mtc0 $t0, $12 
+        nop
+        nop
+        nop
+
+	li $t0, 0
+	mtc1 $t0, $f2
+	cvt.d.w $f2, $f2
+	dmfc1 $a0, $f2
+
+	li $t0, 1
+	mtc1 $t0, $f2
+	cvt.d.w $f2, $f2
+	dmfc1 $a1, $f2
+
+	lui $t0, 0xffff
+	ori $t0, $t0, 0xffff
+	mtc1 $t0, $f2
+	cvt.d.w $f2, $f2
+	dmfc1 $a2, $f2
+
+	# Dump registers on the simulator (gxemul dumps regs on exit)
+	mtc0 $at, $26
+	nop
+	nop
+
+	# Terminate the simulator
+	mtc0 $at, $23
+end:
+	b end
+	nop
+
+.end start
