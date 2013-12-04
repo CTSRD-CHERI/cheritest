34b6b5b41ed7767a75c93c5396e4ec60f123e8e6
mr101 <mr101@cl.cam.ac.uk>
2013-12-04 14:29:01 +0000

    test_clang_opaque.c now uses compiler built-ins to explicit seal and unseal capabilities, rather than using #pragma opaque and getting the compiler to automatically insert the seal/unseal operations.
    Former-commit-id: 503690c3f3feecc6e0365d0c9336a72cda7f68bd

--- tests/c/test_clang_opaque.c
+++ tests/c/test_clang_opaque.c
@@ -40,28 +40,24 @@ typedef __capability struct example *example_t;
 
 static __capability void *example_key;
 
-#pragma opaque example_t example_key
+/* If we used the following declaration, the compiler would automatically
+ * insert calls to csealdata and cunseal. Instead, we explicitly seal and
+ * unseal using compiler built-ins.
+ *
+ * #pragma opaque example_t example_key
+ */
 
 static struct example example_object = {0};
 
 static char *entry[] = {0};
 
-#if 0
-/* extern __capability void *cheri_create_template(size_t offset); */
-#endif
-
 void example_init(void)
 {
 /*
  * example_key will be used to seal and unseal variables of type example_t.
+ * Initialize its otype field to &entry, like this: 
  */
-#if 0
-  /* Eventually, we'll initialize example_key like this: */
-  /* example_key = cheri_create_template(0x1234); */
-#else
-  /* For now, initialize its otype field to &entry, like this: */
   example_key = __builtin_cheri_set_cap_type((__capability void *) entry, 0);
-#endif
 }
 
 example_t example_constructor(void)
@@ -79,24 +75,18 @@ example_t example_constructor(void)
   result = (example_t) __builtin_cheri_and_cap_perms((__capability void *) ptr,
     0xd);
 
+  result = __builtin_cheri_seal_cap_data(result, example_key);
+
   return result;
 }
 
 int example_method(example_t o)
 {
-  o->x++;
-  return o->x;
-}
+example_t p;
 
-static void invoke_callback()
-{
-  example_t e;
-  int rc;
-
-  e = (example_t) &example_object;
-  rc = example_method(e);
-  // Comment out unti test is expected to pass
-  //assert(e->x == 1);
+  p = __builtin_cheri_unseal_cap(o, example_key);
+  p->x++;
+  return p->x;
 }
 
 int test(void)
@@ -106,14 +96,8 @@ int r;
 
   example_init();
   e = example_constructor();
-  // Comment out unti test is expected to pass
-  //assert(e->x == 0);
-  invoke_callback();
-
-/*
   r = example_method(e);
-  assert(e->x == 1);
-*/
+  assert(r == 1);
 
   return 0;
 }
