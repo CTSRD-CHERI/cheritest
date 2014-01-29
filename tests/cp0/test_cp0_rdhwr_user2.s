d56a5f593e60946bce441426b425a0f413e8a73a
mr101 <mr101@cl.cam.ac.uk>
2014-01-29 16:10:49 +0000

    Added a test for using the rdhwr instruction from user space.
    Former-commit-id: 3460771ac5fac46038e349b62d66b38cbe31b73f

---
+++ tests/cp0/test_cp0_rdhwr_user2.s
@@ -0,0 +1,123 @@
+#-
+# Copyright (c) 2012 Robert M. Norton
+# Copyright (c) 2014 Michael Roe
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+# OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+# OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
+
+# Test that the rdhwr instruction can be used in userspace when the right
+# bit in CP0.HWREna is set.
+
+.set mips64
+.set noreorder
+.set nobopt
+
+.global test
+test:   .ent    test
+		daddu	$sp, $sp, -16
+		sd	$ra, 8($sp)
+		sd	$fp, 0($sp)
+		daddu	$fp, $sp, 16
+
+		jal     bev_clear
+		nop
+		
+		#
+		# Install exception handler
+		#
+
+		dla	$a0, exception_handler
+		jal 	bev0_handler_install
+		nop
+
+		#
+		# Set CP0.HWREna.userlocal, enabling access to the userlocal
+		# register from userspace.
+		#
+
+		dli	$t0, 1 << 29
+		dmtc0	$t0, $7
+
+		# UserLocal is readable as hardware register 29
+		# and writable as CP0 register 4, select 2.
+
+		lui 	$t0, 0x1234
+		ori	$t0, $t0, 0x5678
+		dsll	$t0, $t0, 16
+		ori	$t0, $t0, 0x9abc
+		dsll	$t0, $t0, 16
+		ori	$t0, 0xdef0
+		dmtc0	$t0, $4, 2
+
+		dli	$a1, 0
+
+                # To test user code we must set up a TLB entry.
+	
+ 		dmtc0	$zero, $5               # Write 0 to page mask i.e. 4k pages
+		dmtc0	$zero, $0		# TLB index 
+		dmtc0	$zero, $10		# TLB entryHi
+
+		dla     $a0, testcode		# Load address of testcode
+		and     $a2, $a0, 0xffffffe000	# Get physical page (PFN) of testcode (40 bits less 13 low order bits)
+		dsrl    $a3, $a2, 6		# Put PFN in correct position for EntryLow
+		or      $a3, 0x13   		# Set valid and global bits, uncached
+		dmtc0	$a3, $2			# TLB EntryLow0
+		daddu 	$a4, $a3, 0x40		# Add one to PFN for EntryLow1
+		dmtc0	$a4, $3			# TLB EntryLow1
+		tlbwi				# Write Indexed TLB Entry
+
+		dli	$a5, 0			# Initialise test flag
+	
+		and     $k0, $a0, 0xfff		# Get offset of testcode within page.
+	        dmtc0   $k0, $14		# Put EPC
+                dmfc0   $t2, $12                # Read status
+                ori     $t2, 0x12               # Set user mode, exl
+                and     $t2, 0xffffffffefffffff # Clear cu0 bit
+                dmtc0   $t2, $12                # Write status
+                nop
+                nop
+	        eret                            # Jump to test code
+                nop
+                nop
+
+the_end:	
+		ld	$ra, 8($sp)
+		ld	$fp, 0($sp)
+		jr      $ra
+		daddu	$sp, $sp, 16
+.end    test
+	
+testcode:
+		nop
+		add	$a5, 1			# Set the test flag
+		.set push
+		.set mips32r2
+		rdhwr 	$a1, $29
+		.set pop
+		syscall 0			# Return to kernel mode
+
+exception_handler:
+                dmfc0   $a6, $12                # Read status
+                dmfc0   $a7, $13                # Read cause
+		dla	$t0, the_end
+		jr	$t0
+		nop
