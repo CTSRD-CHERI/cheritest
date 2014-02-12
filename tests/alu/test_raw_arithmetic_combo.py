7d96a6eba3e7b4335761fefc28df5a88f0c80869
mr101 <mr101@cl.cam.ac.uk>
2014-02-12 12:11:16 +0000

    Fixed a typo in test_raw_arithmetic_combo.py.
    Former-commit-id: d74cffc1b652a02229fb3796982ea3f3045f4657

--- tests/alu/test_raw_arithmetic_combo.py
+++ tests/alu/test_raw_arithmetic_combo.py
@@ -31,7 +31,7 @@ from cheritest_tools import BaseCHERITestCase
 
 class test_raw_arithmetic_combo(BaseCHERITestCase):
     def test_a0(self):
-        '''Test that stages of the arithmetic combo test procuced the correct result.'''
+        '''Test that stages of the arithmetic combo test produced the correct result.'''
         self.assertRegisterEqual(self.MIPS.s0, 0x0000002800000014, "Stage 1 Incorrect")
         self.assertRegisterEqual(self.MIPS.s1, 0x0000000000000001, "Stage 2 Incorrect")
         self.assertRegisterEqual(self.MIPS.s2, 0xffffffff9c320384, "Stage 3 Incorrect")
