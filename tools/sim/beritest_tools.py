e03c364b3061a1e87b58ae10ed4ad1c3fb2f64a5
rnw24 <rnw24@cl.cam.ac.uk>
2014-03-01 23:50:48 +0000

    Remove excess parens in some license headers.
    M    cheritest/trunk/tests/cache/test_cache_instruction_instruction.py
    M    cheritest/trunk/tests/cache/test_cache_instruction_instruction.s
    M    cheritest/trunk/tests/multicore/test_raw_coherence_mp_loop.py
    M    cheritest/trunk/tests/multicore/test_raw_coherence_mp_loop.s
    M    cheritest/trunk/tests/multicore/test_raw_coherence_sequential.py
    M    cheritest/trunk/tests/multicore/test_raw_coherence_sequential.s
    M    cheritest/trunk/tests/multicore/test_raw_coherence_setup.py
    M    cheritest/trunk/tests/multicore/test_raw_coherence_setup.s
    M    cheritest/trunk/tests/multicore/test_raw_coherent_sync.py
    M    cheritest/trunk/tests/multicore/test_raw_coherent_sync.s
    M    cheritest/trunk/tests/multicore/test_raw_pic_default.py
    M    cheritest/trunk/tests/multicore/test_raw_pic_default.s
    M    cheritest/trunk/tools/sim/__init__.py
    M    cheritest/trunk/tools/sim/beritest_tools.py
    M    cherilibs/trunk/AsymmetricBRAM/AsymmetricBRAM.bsv
    M    cherilibs/trunk/AsymmetricBRAM/Tb1.bsv
    M    cherilibs/trunk/AsymmetricBRAM/Tb2.bsv
    M    cherilibs/trunk/AsymmetricBRAM/Tb3.bsv
    M    cherilibs/trunk/BRAMBanked.bsv
    M    cherilibs/trunk/L2Cache.bsv
    M    cherilibs/trunk/MemTypes.bsv
    M    cherilibs/trunk/MultiPortL2Cache.bsv
    M    cherilibs/trunk/PIC.bsv
    M    cherilibs/trunk/PISM.bsv
    M    cherilibs/trunk/Processor.bsv
    M    cherilibs/trunk/TraceUnit.bsv
    M    cherilibs/trunk/UnitTesting.bsv
    M    cherilibs/trunk/debugging/Assoc.bsv
    M    cherilibs/trunk/debugging/Avalon2ClientServer.bsv
    M    cherilibs/trunk/debugging/AvalonTrace.bsv
    M    cherilibs/trunk/debugging/BurstServer.bsv
    M    cherilibs/trunk/debugging/DebugModule.bsv
    M    cherilibs/trunk/debugging/DynamicBits.bsv
    M    cherilibs/trunk/debugging/Makefile
    M    cherilibs/trunk/include/cheri_debug.h
    M    cherilibs/trunk/peripherals/pismdev/chericonf.c
    M    cherilibs/trunk/peripherals/pismdev/config.y
    M    cherilibs/trunk/peripherals/pismdev/dram/dram.c
    M    cherilibs/trunk/peripherals/pismdev/pism.c
    M    cherilibs/trunk/peripherals/pismdev/pism.h
    M    cherilibs/trunk/peripherals/pismdev/pism_device.c
    M    cherilibs/trunk/peripherals/pismdev/pismtest.c
    M    cherilibs/trunk/peripherals/pismdev/sdcard/sdcard.c
    M    cherilibs/trunk/tools/debug/altera_systemconsole.c
    M    cherilibs/trunk/tools/debug/berictl.c
    M    cherilibs/trunk/tools/debug/cheri_atse.c
    M    cherilibs/trunk/tools/debug/cheri_debug.c
    M    cherilibs/trunk/tools/debug/cheri_err.c
    M    cherilibs/trunk/tools/debug/cheri_pic.c
    M    cherilibs/trunk/tools/debug/cheri_systemconsole.c
    M    cherilibs/trunk/tools/debug/cherictl.c
    M    cherilibs/trunk/tools/debug/cherictl.h
    M    cherilibs/trunk/tools/debug/cherictl_base.c
    M    cherilibs/trunk/unit_tests/Makefile
    M    cherilibs/trunk/unit_tests/TestAsymmetricBRAM.bsv
    M    cherilibs/trunk/unit_tests/TestBeriUGBypassFIFOF.bsv
    M    cheri/trunk/Branch.bsv
    M    cheri/trunk/CP0.bsv
    M    cheri/trunk/CP0Micro.bsv
    M    cheri/trunk/DCache.bsv
    M    cheri/trunk/Decode.bsv
    M    cheri/trunk/Execute.bsv
    M    cheri/trunk/GenericDCache.bsv
    M    cheri/trunk/GenericICache.bsv
    M    cheri/trunk/ICache.bsv
    M    cheri/trunk/MIPS.bsv
    M    cheri/trunk/MIPSTop.bsv
    M    cheri/trunk/Makefile
    M    cheri/trunk/Memory.bsv
    M    cheri/trunk/Multicore.bsv
    M    cheri/trunk/Proc.bsv
    M    cheri/trunk/Scheduler.bsv
    M    cheri/trunk/StoreConditional.bsv
    M    cheri/trunk/Writeback.bsv
    M    cheri/trunk/boards/terasic_de4/DE4_TOP.v
    M    cheri/trunk/setup.sh
    M    cheri/trunk/sw/fpu_tests/init.c
    M    cheri/trunk/sw/helloworld/test.c
    M    cheri2/trunk/setup.sh
    
    
    
    Former-commit-id: f9dfd9c61b691efd4ddc9601f44a06aa78c4c1f9

--- tools/sim/beritest_tools.py
+++ tools/sim/beritest_tools.py
@@ -5,11 +5,11 @@
 # All rights reserved.
 #
 # This software was developed by SRI International and the University of
-# Cambridge Computer Laboratory under DARPA/AFRL contract (FA8750-10-C-0237)
+# Cambridge Computer Laboratory under DARPA/AFRL contract FA8750-10-C-0237
 # ("CTSRD"), as part of the DARPA CRASH research programme.
 #
 # This software was developed by SRI International and the University of
-# Cambridge Computer Laboratory under DARPA/AFRL contract (FA8750-11-C-0249)
+# Cambridge Computer Laboratory under DARPA/AFRL contract FA8750-11-C-0249
 # ("MRC2"), as part of the DARPA MRC research programme.
 #
 # @BERI_LICENSE_HEADER_START@
