e7fbf4db09e1bce55eb98ca72cf310078dd4bb52
mr101 <mr101@cl.cam.ac.uk>
2014-04-02 13:13:15 +0000

    test_id_coherence: tag this test with 'cache' (test assumes CPU has a memory cache) and 'cached' (test must be run from cached memory). The MIPS spec says that a CACHE operation on uncached memory results in "undefined" behavior.
    Former-commit-id: 5ceb101e9678f0d277fa6ccb45c4246d19699403

--- tests/cache/test_id_coherence.py
+++ tests/cache/test_id_coherence.py
@@ -30,5 +30,7 @@ from nose.plugins.attrib import attr
 
 class test_id_coherence(BaseBERITestCase):
 
+    @attr('cache')
+    @attr('cached')
     def test_id_coherence_1(self):
         self.assertRegisterEqual(self.MIPS.a1, 0x78, "Self-modifying code did not compute sum correctly")
