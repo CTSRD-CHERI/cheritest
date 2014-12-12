a595c659c6e5049b8736d9274bc8b29fc17a4a54
mr101 <mr101@cl.cam.ac.uk>
2014-12-12 18:30:31 +0000

    test_cache_instruction_L2: Removed the part of the test that read a tag from the cache. This was failing (due to a change in the cache from direct mapped to associative), and has been replaced by test_cache_taglo.
    (The new test is testing somewhat different functionality from the old one).
    
    
    
    
    Former-commit-id: 6c4e25df842797b16f7e6dc7af01034a8cf0cce8

--- tests/cache/test_cache_instruction_L2.py
+++ tests/cache/test_cache_instruction_L2.py
@@ -78,7 +78,3 @@ class test_cache_instruction_L2(BaseBERITestCase):
     def test_invalidated_writable_location(self):
         self.assertRegisterEqual(self.MIPS.a6, 5, "Writable location not invalidated")
 
-    @attr('cache')
-    @attr('loadcachetag')
-    def test_index_load_tag_l2cache(self):
-        self.assertRegisterEqual(self.MIPS.a7, 0x0000000040004000, "Index load tag l2 unexpected value")
