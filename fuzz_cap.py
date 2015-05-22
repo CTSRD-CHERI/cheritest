c08d96938a0fbf3bac72b4edaaf2b270aead4f47
mn416 <mn416@cl.cam.ac.uk>
2015-05-22 12:34:55 +0000

    Add license headers
    Former-commit-id: bfc4ea2c7c5a1e6fe0b81efd6fac51a88e1313ce

--- fuzz_cap.py
+++ fuzz_cap.py
@@ -1,7 +1,32 @@
 #!/usr/bin/env python
 
-# Matt N, 5 May 2015
 #
+# Copyright (c) 2015 Matthew Naylor
+# All rights reserved.
+#
+# This software was developed by SRI International and the University of
+# Cambridge Computer Laboratory under DARPA/AFRL contract FA8750-10-C-0237
+# ("CTSRD"), as part of the DARPA CRASH research programme.
+#
+# @BERI_LICENSE_HEADER_START@
+#
+# Licensed to BERI Open Systems C.I.C. (BERI) under one or more contributor
+# license agreements.  See the NOTICE file distributed with this work for
+# additional information regarding copyright ownership.  BERI licenses this
+# file to you under the BERI Hardware-Software License, Version 1.0 (the
+# "License"); you may not use this file except in compliance with the
+# License.  You may obtain a copy of the License at:
+#
+#   http://www.beri-open-systems.org/legal/license-1-0.txt
+#
+# Unless required by applicable law or agreed to in writing, Work distributed
+# under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+# CONDITIONS OF ANY KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations under the License.
+#
+# @BERI_LICENSE_HEADER_END@
+#
+
 # Generate random capability-instruction sequences and check for
 # equivalance between model (L3) and implementation (BSV).  This script
 # must be run in the 'ctsrd/cheritest/trunk/' directory.  The other
@@ -501,10 +526,10 @@ try:
   else:
     seed = int(options.seed)
 
-  print ("Setting random seed to %i" % seed)
   random.seed(seed)
 
   if options.filename == None:
+    print ("Setting random seed to %i" % seed)
     for i in range (1,numTests):
       sys.stdout.write("\rTest %i/%i" % (i, numTests))
       sys.stdout.flush()
