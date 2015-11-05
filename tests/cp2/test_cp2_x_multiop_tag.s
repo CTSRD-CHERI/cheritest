6c8698ad13e4eb95c184349a00af29a87f085b82
mr101 <mr101@cl.cam.ac.uk>
2015-11-05 14:00:53 +0000

    test_cp2_x_multiop_tag: Added a test of CLLC/CSCC via a capability that does not have the tag bit set.
    Former-commit-id: 554590fbd724bc1f75036d75fc57efc92d51fb30

--- tests/cp2/test_cp2_x_multiop_tag.s
+++ tests/cp2/test_cp2_x_multiop_tag.s
@@ -139,6 +139,8 @@ test:		.ent test
 		cldr	$t0, $t1($c1)
 		csdr	$t0, $t1($c1)
 		csetoffset $c1, $c1, $t1
+		cllc	$c2, $c1
+		cscc	$t0, $c1, $c1
 		cllb	$t0, $c1
 		cscb	$t0, $t0, $c1
 		cllh	$t0, $c1
