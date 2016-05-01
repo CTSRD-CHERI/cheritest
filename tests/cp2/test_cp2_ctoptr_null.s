706403e5e4d98a56f82f995b3f32edfa7ee017fd
mr101 <mr101@cl.cam.ac.uk>
2016-05-01 14:10:45 +0000

    test_cp2_ctoptr_null: Use CSetBounds rather than CIncBase.
    Former-commit-id: 74f7d8a67d2507242930079fd484e3f8cc106e2a

--- tests/cp2/test_cp2_ctoptr_null.s
+++ tests/cp2/test_cp2_ctoptr_null.s
@@ -43,8 +43,12 @@ test:		.ent test
 		daddu	$fp, $sp, 32
 
 
+		cgetdefault $c1
 		dli	$t0, 4
-		cincbase $c1, $c0, $t0
+		csetoffset $c1, $c1, $t0
+		dli	$t0, 4
+		csetbounds $c1, $c1, $t0
+
 		cfromptr $c2, $c0, $zero
 		dli	$a0, 1
 		ctoptr	$a0, $c2, $c1
