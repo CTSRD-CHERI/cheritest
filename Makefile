3076dbe1cf8b562f38da84eeaf3f40c1b696d627
mr101 <mr101@cl.cam.ac.uk>
2017-05-11 23:00:06 +0000

    cheritest: Added a test for floating point comparisons storing the result in a condition code other than $fcc0. (This is a MIPS IV feature).
    Former-commit-id: 724b61e89ea7738df7667d122873fb2c75175742

--- Makefile
+++ Makefile
@@ -688,6 +688,7 @@ TEST_CP0_FILES=					\
 
 TEST_FPU_FILES=					\
 		test_fpu_exception_pipeline.s	\
+		test_fpu_compare_cc.s		\
 		test_fpu_x_div.s		\
 		test_fpu_x_underflow.s		\
 		test_fpu_x_overflow.s		\
---
+++ tests/fpu/test_fpu_compare_cc.s
@@ -0,0 +1,65 @@
+#-
+# Copyright (c) 2017 Michael Roe
+# All rights reserved.
+#
+# This software was developed by the University of Cambridge Computer
+# Laboratory as part of the Rigorous Engineering of Mainstream Systems (REMS)
+# project, funded by EPSRC grant EP/K008528/1.
+#
+# @BERI_LICENSE_HEADER_START@
+#
+# Licensed to BERI Open Systems C.I.C. (BERI) under one or more contributor
+# license agreements.  See the NOTICE file distributed with this work for
+# additional information regarding copyright ownership.  BERI licenses this
+# file to you under the BERI Hardware-Software License, Version 1.0 (the
+# "License"); you may not use this file except in compliance with the
+# License.  You may obtain a copy of the License at:
+#
+#   http://www.beri-open-systems.org/legal/license-1-0.txt
+#
+# Unless required by applicable law or agreed to in writing, Work distributed
+# under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+# CONDITIONS OF ANY KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations under the License.
+#
+# @BERI_LICENSE_HEADER_END@
+#
+
+.set mips64
+.set noreorder
+.set nobopt
+.set noat
+
+#
+# Test floating point comparisons can store the result in condition codes
+# other than $fcc0. (This feature was added in MIPS IV).
+#
+
+		.global test
+test:		.ent test
+		daddu 	$sp, $sp, -32
+		sd	$ra, 24($sp)
+		sd	$fp, 16($sp)
+		daddu	$fp, $sp, 32
+
+		mfc0 $at, $12
+		dli $t1, 1 << 29	# Enable CP1
+		or $at, $at, $t1
+		dli $t1, 1 << 26	# Put FPU into 64 bit mode
+		or $at, $at, $t1
+		mtc0 $at, $12 
+
+		dmtc1	$zero, $f0
+		ctc1	$zero, $f25
+		c.eq.s	$fcc1, $f0, $f0
+		nop
+		nop
+		nop
+		cfc1	$a0, $f25
+
+		ld	$fp, 16($sp)
+		ld	$ra, 24($sp)
+		daddu	$sp, $sp, 32
+		jr	$ra
+		nop			# branch-delay slot
+		.end	test
