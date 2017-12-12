fa4c8c4a2716fece75abc6a391ed1bc3cf9d0e05
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2017-12-12 16:50:04 +0000

    Update assertNullCap() to check for length == -1
    Now that QEMU has also been updated we should make this change to get
    Jenkins back into a mostly green state.
    
    This brings the QEMU tests down to 1 failure due to mtc0 sign extension as
    well as two NAN2008 related FPU tests

--- tools/sim/beritest_tools.py
+++ tools/sim/beritest_tools.py
@@ -303,7 +303,8 @@ class BaseBERITestCase(unittest.TestCase):
         self.assertRegisterEqual(cap.perms , 0, msg + "has nonzero perms")
         self.assertRegisterEqual(cap.offset, 0, msg + "has nonzero offset")
         self.assertRegisterEqual(cap.base  , 0, msg + "has nonzero base")
-        self.assertRegisterEqual(cap.length, 0, msg + "has nonzero length")
+        length_of_null = 0xffffffffffffffff
+        self.assertRegisterEqual(cap.length, length_of_null, msg + "length is not -1")
 
     def assertDefaultCap(self, cap, msg="", perms=None):
         self.assertValidCap(cap, msg, offset=0, base=0, length=self.max_length, perms=perms,
