3b65b6be286875150a77793eefc0e83122ab4b9c
Michael Roe <mroe@cornstalk.org.uk>
2018-04-21 19:53:54 +0100

    Added a test for CSetBoundsExact raising a length exception.

---
+++ tests/cp2/test_cp2_x_csetboundsexact_length.s
@@ -0,0 +1,123 @@
+#-
+# Copyright (c) 2012, 2015, 2018 Michael Roe
+# All rights reserved.
+#
+# This software was developed by SRI International and the University of
+# Cambridge Computer Laboratory under DARPA/AFRL contract FA8750-10-C-0237
+# ("CTSRD"), as part of the DARPA CRASH research programme.
+#
+# @BERI_LICENSE_HEADER_START@
+#
+# Licensed to BERI Open Systems C.I.C. (BERI) under one or more contributor
+# license agreements.  See the NOTICE file distributed with this work for
+# additional information regarding copyright ownership.  BERI licenses this
+# file to you under the BERI Hardware-Software License, Version 1.0 (the
+# "License"); you may not use this file except in compliance with the
+# License.  You may obtain a copy of the License at:
+#
+#   http://www.beri-open-systems.org/legal/license-1-0.txt
+#
+# Unless required by applicable law or agreed to in writing, Work distributed
+# under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+# CONDITIONS OF ANY KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations under the License.
+#
+# @BERI_LICENSE_HEADER_END@
+#
+
+.include "macros.s"
+.set mips64
+.set noreorder
+.set nobopt
+.set noat
+
+#
+# Test that CSetBoundsExact raises an exception if the requested length is too
+# large.
+#
+
+BEGIN_TEST
+		#
+		# Set up exception handler
+		#
+
+		jal	bev_clear
+		nop
+		dla	$a0, bev0_handler
+		jal	bev0_handler_install
+		nop
+
+		#
+		# $a2 will be set to 1 if the exception handler is called
+		#
+
+		dli	$a2, 0
+
+		#
+		# $a3 will be set to the final length, if an exception isn't
+		# raised
+		#
+
+		dli	$a3, 0
+
+		#
+		# Make $c1 a capability for the array 'data'
+		#
+
+		cgetdefault $c1
+		dla	$t0, data
+		csetoffset $c1, $c1, $t0
+		dli	$t0, 8
+		csetbounds $c1, $c1, $t0
+
+		#
+		# Save the actual length (might not be the requested length
+		# if capabilities are imprecise, although it probably will
+		# be).
+		#
+
+		cgetlen $a0, $c1
+
+		#
+		# Set the length to zero (will almost certainly be
+		# representable, even though the spec doesn't guarantee it).
+		#
+
+		csetbounds $c1, $c1, $zero
+
+		#
+		# Check what length we got
+		#
+
+		cgetlen $a1, $c1
+
+		#
+		# Try to increase the bounds back
+		#
+
+		csetboundsexact $c1, $c1, $a0 # This should raise an exception
+
+		cgetlen	$a3, $c1
+
+END_TEST
+
+		.ent bev0_handler
+bev0_handler:
+		li	$a2, 1
+		cgetcause $a3
+		dmfc0	$a5, $14	# EPC
+		daddiu	$k0, $a5, 4	# EPC += 4 to bump PC forward on ERET
+		dmtc0	$k0, $14
+		nop
+		nop
+		nop
+		nop
+		eret
+		.end bev0_handler
+
+		.data
+		.align	3
+data:		.dword	0x0123456789abcdef
+		.dword  0x0123456789abcdef
+
+
