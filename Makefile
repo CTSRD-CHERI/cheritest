0fdb88713089fdd538968906c515b373cfe97568
Michael Roe <mroe@cornstalk.org.uk>
2018-04-21 20:12:13 +0100

    Added another test for CTestSubset.

--- Makefile
+++ Makefile
@@ -1039,6 +1039,7 @@ TEST_CP2_FILES=					\
 		test_cp2_ccopytype.s		\
 		test_cp2_cbuildcap.s		\
 		test_cp2_cllc_perm_tag.s	\
+		test_cp2_ctestsubset_tag.s	\
 		test_cp2_x_bounds.s		\
 		test_cp2_x_clbu_tag.s		\
 		test_cp2_x_clbu_reg.s		\
---
+++ tests/cp2/test_cp2_ctestsubset_tag.s
@@ -0,0 +1,71 @@
+#-
+# Copyright (c) 2018 Michael Roe
+# All rights reserved.
+#
+# This software was developed by the University of Cambridge Computer
+# Laboratory as part of the Rigorous Engineering of Mainstream Systems (REMS)
+# project, funded by EPSRC grant EP/K008528/1.
+#
+# @BERI_LICENSE_HEADER_START@
+#
+# Licensed to BERI Open Systems C.I.C. (BERI) under one or more contributor
+# license agreements.  See the NOTICE file distributed with this work for
+# additional information regarding copyright ownership.  BERI licenses this
+# file to you under the BERI Hardware-Software License, Version 1.0 (the
+# "License"); you may not use this file except in compliance with the
+# License.  You may obtain a copy of the License at:
+#
+#   http://www.beri-open-systems.org/legal/license-1-0.txt
+#
+# Unless required by applicable law or agreed to in writing, Work distributed
+# under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+# CONDITIONS OF ANY KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations under the License.
+#
+# @BERI_LICENSE_HEADER_END@
+#
+
+.set mips64
+.set noreorder
+.set nobopt
+.set noat
+.include "macros.s"
+
+#
+# Test CTestSubset with different tag bits and sealed bits.
+#
+
+BEGIN_TEST
+
+		dla	$t0, data
+		cgetdefault $c1
+		csetoffset $c1, $c1, $t0
+		dli	$t0, 8
+		csetbounds $c1, $c1, $t0
+
+		ccleartag $c2, $c1
+
+		ctestsubset $a0, $c2, $c1
+
+		ctestsubset $a1, $c1, $c2
+
+		cgetdefault $c1
+		# FIXME: ought to have a better way to get a sealable set
+		# of bounds
+		dli 	$t0, 0x9800001234567000
+		csetoffset $c1, $c1, $t0
+		dli	$t0, 0x1000
+		csetbounds $c1, $c1, $t0
+
+		cgetdefault $c3
+		dli	$t0, 0x1234
+		csetoffset $c3, $c3, $t0
+
+		cseal $c2, $c1, $c3
+
+		ctestsubset $a2, $c1, $c2
+		ctestsubset $a3, $c2, $c1
+END_TEST
+
+		.data
+data:		.dword 0
