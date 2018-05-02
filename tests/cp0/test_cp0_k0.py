9a67370fe288696d3602958024dcf4c3becf11cb
Michael Roe <mroe@cornstalk.org.uk>
2018-05-02 21:59:21 +0100

    Added a test for setting Config.K0 (kseg0 coherency algorithm).

---
+++ tests/cp0/test_cp0_k0.py
@@ -0,0 +1,38 @@
+#-
+# Copyright (c) 2018 Michael Roe
+#
+# This software was developed by the University of Cambridge Computer
+# Laboratory as part of the Rigorous Engineering of Mainstream Systems (REMS)
+# project, funded by EPSRC grant EP/K008528/1.
+#
+# @BERI_LICENSE_HEADER_START@
+#
+# Licensed to BERI Open Systems C.I.C. (BERI) under one or more contributor
+# license agreements.  See the NOTICE file distributed with this work for
+# additional information regarding copyright ownership.  BERI licenses this
+# file to you under the BERI Hardware-Software License, Version 1.0 (the
+# "License"); you may not use this file except in compliance with the
+# License.  You may obtain a copy of the License at:
+#
+#   http://www.beri-open-systems.org/legal/license-1-0.txt
+#
+# Unless required by applicable law or agreed to in writing, Work distributed
+# under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+# CONDITIONS OF ANY KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations under the License.
+#
+# @BERI_LICENSE_HEADER_END@
+#
+
+from beritest_tools import BaseBERITestCase
+from nose.plugins.attrib import attr
+
+class test_cp0_k0(BaseBERITestCase):
+
+    def test_cp0_k0_1(self):
+        '''Test we can set Config.K0 to uncached'''
+        self.assertRegisterMaskEqual(self.MIPS.a1, 0x3, 2, "Config0.K0 was not set to 2 (uncached)")
+
+    def test_cp0_k0_2(self):
+        '''Test we can set Config.K0 to cacheable noncoherent'''
+        self.assertRegisterMaskEqual(self.MIPS.a2, 0x3, 3, "Config0.K0 was not set to 3 (cacheable noncoherent)")
---
+++ tests/cp0/test_cp0_k0.s
@@ -0,0 +1,53 @@
+#-
+# Copyright (c) 2018 Michael Roe
+# All rights reserved.
+#
+# This software was developed by the University of Cambridge Computer
+# Laboratory as part of the Rigorous Engineering of Mainstream Systems (REMS)
+# project, funded by EPSRC grant EP/K008528/1.
+#
+# @BERI_LICENSE_HEADER_START@
+#
+# Licensed to BERI Open Systems C.I.C. (BERI) under one or more contributor
+# license agreements.  See the NOTICE file distributed with this work for
+# additional information regarding copyright ownership.  BERI licenses this
+# file to you under the BERI Hardware-Software License, Version 1.0 (the
+# "License"); you may not use this file except in compliance with the
+# License.  You may obtain a copy of the License at:
+#
+#   http://www.beri-open-systems.org/legal/license-1-0.txt
+#
+# Unless required by applicable law or agreed to in writing, Work distributed
+# under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+# CONDITIONS OF ANY KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations under the License.
+#
+# @BERI_LICENSE_HEADER_END@
+#
+
+.set mips64
+.set noreorder
+.set nobopt
+.set noat
+.include "macros.s"
+
+#
+# Test we can set Config.K0 (kseg0 coherency algorithm)
+#
+
+BEGIN_TEST
+
+	mfc0	$a0, $16	# Config register
+	dli	$t0, 3
+	nor	$t0, $t0, $t0
+	and	$a0, $a0, $t0
+
+	ori	$t0, $a0, 2
+	mtc0	$t0, $16
+	mfc0	$a1, $16
+
+	ori	$t0, $a0, 3
+	mtc0	$t0, $16
+	mfc0	$a2, $16
+
+END_TEST
