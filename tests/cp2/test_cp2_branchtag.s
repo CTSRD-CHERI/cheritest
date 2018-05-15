23e4828237ee203bbd997fe2068ab818743cd90d
Michael Roe <mroe@cornstalk.org.uk>
2018-05-15 19:10:29 +0100

    Reformatted to match indentation/capitalization of the rest of the tests.

--- tests/cp2/test_cp2_branchtag.s
+++ tests/cp2/test_cp2_branchtag.s
@@ -40,46 +40,46 @@
 .global test
 test:
 .ent test
-	dli       $a0, 0
-	dli       $a1, 0
+		dli       $a0, 0
+		dli       $a1, 0
 
-	# c0 should have a valid cap, c1 an invalid one
-	ccleartag $c1, $c0
+		# c0 should have a valid cap, c1 an invalid one
+		ccleartag $c1, $c0
 
-	# Check that the branches are taken when they should be and that their
-	# delay slots execute.
-	CBTS      $c0, clear1
-	daddi     $a0, $a0, 1
+		# Check that the branches are taken when they should be and that their
+		# delay slots execute.
+		cbts      $c0, clear1
+		daddi     $a0, $a0, 1
 cont1:
-	CBTU      $c1, clear2
-	daddi     $a1, $a1, 1
+		cbtu      $c1, clear2
+		daddi     $a1, $a1, 1
 cont2:
-	# Check that the branches are not taken when they shouldn't be and that
-	# their delay slots execute.
-	CBTS      $c1, clear3
-	daddi     $a0, $a0, 2
+		# Check that the branches are not taken when they shouldn't be and that
+		# their delay slots execute.
+		cbts      $c1, clear3
+		daddi     $a0, $a0, 2
 cont3:
-	CBTU      $c0, clear4
-	daddi     $a1, $a1, 2
+		cbtu      $c0, clear4
+		daddi     $a1, $a1, 2
 cont4:
 
-	# By this point, a0 and a1 should both be 0b111 (7).  Each of the delay
-	# slots will set one of the low bits and each of the branch targets will
-	# set one of the next bits, but only one of the targets should be reached.
+		# By this point, a0 and a1 should both be 0b111 (7).  Each of the delay
+		# slots will set one of the low bits and each of the branch targets will
+		# set one of the next bits, but only one of the targets should be reached.
 
-	jr        $ra
-	nop  # branch-delay slot
+		jr        $ra
+		nop  # branch-delay slot
 clear1:
-	b         cont1
-	daddi     $a0, $a0, 4 # in delay slot
+		b         cont1
+		daddi     $a0, $a0, 4 # in delay slot
 clear2:
-	b         cont2
-	daddi     $a1, $a1, 4 # in delay slot
-# Should not be reached:
+		b         cont2
+		daddi     $a1, $a1, 4 # in delay slot
+		# Should not be reached:
 clear3:
-	b         cont3
-	daddi     $a0, $a0, 8 # in delay slot
+		b         cont3
+		daddi     $a0, $a0, 8 # in delay slot
 clear4:
-	b         cont4
-	daddi     $a1, $a1, 8 # in delay slot
+		b         cont4
+		daddi     $a1, $a1, 8 # in delay slot
 .end	test
