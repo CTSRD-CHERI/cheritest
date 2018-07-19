407cbfd68ea1b573f88ff10a7890fe362bd53b0d
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2018-07-19 14:48:37 +0100

    Further increase the test_cp0_rdhwr_user2_count_qemu range
    QEMU built with debug + ASAN + UBSAN is really slow so this can fail there.

--- tests/cp0/test_cp0_rdhwr_user2.py
+++ tests/cp0/test_cp0_rdhwr_user2.py
@@ -54,5 +54,5 @@ class test_cp0_rdhwr_user2(BaseBERITestCase):
     @attr('count_register_is_time')
     def test_cp0_rdhwr_user2_count_qemu(self):
         '''Test that the user count register can be read from user space'''
-        self.assertRegisterInRange(self.MIPS.a3, self.MIPS.a2, self.MIPS.a2 + 800000,
+        self.assertRegisterInRange(self.MIPS.a3, self.MIPS.a2, self.MIPS.a2 + 8000000,
                                    "rdhwr counter did not approximately match cp0 count ")
