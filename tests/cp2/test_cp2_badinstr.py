ca5d272d0cf5a15c8415de5229fee4c98fc15112
Robert Norton <rmn30@cam.ac.uk>
2018-09-14 15:11:50 +0100

    Expect sign extended badinstr from mfc0 as per MIPS spec. Doesn't make much sense so should probably use dmfc0 in practice.

--- tests/cp2/test_cp2_badinstr.py
+++ tests/cp2/test_cp2_badinstr.py
@@ -48,8 +48,8 @@ class test_cp2_badinstr(BaseBERITestCase):
         self.assertRegisterEqual(self.MIPS.s1, 0xe8930803, "expected csd  $4, $1, 0($c19) in BadInstr")
 
     def test_badinstr_value_mfc0(self):
-        # mfc0 should not sign extend the BadInstr value
-        self.assertRegisterEqual(self.MIPS.s2, 0xe8930803, "expected csd  $4, $1, 0($c19) in BadInstr (not sign extended)")
+        # mfc0 should sign extend the BadInstr value
+        self.assertRegisterEqual(self.MIPS.s2, 0xffffffffe8930803, "expected csd  $4, $1, 0($c19) in BadInstr (sign extended)")
 
     def test_value_not_written(self):
         assert self.MIPS.s0 == 0x123456
