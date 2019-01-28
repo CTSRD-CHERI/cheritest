6d128e041b0ebe2899ad945a06c9ca4b69d5f44e
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2019-01-28 11:10:43 +0000

    Allow hwreg 28 to exist

--- tests/cp2/test_cp2_reg_init.py
+++ tests/cp2/test_cp2_reg_init.py
@@ -49,30 +49,25 @@ import os
 #    registers, which is highly desirable for the general-purpose range.
 #
 
+@attr('capabilities')
 class test_cp2_reg_init(BaseBERITestCase):
 
-    @attr('capabilities')
     def test_cp2_reg_init_pcc_base(self):
         '''Test that CP2 register PCC.base is correctly initialised'''
         self.assertRegisterEqual(self.MIPS.pcc.base, 0x0, "CP2 PCC base incorrectly initialised")
 
-    @attr('capabilities')
     def test_cp2_reg_init_pcc_len(self):
         self.assertRegisterEqual(self.MIPS.pcc.length, 0xffffffffffffffff, "CP2 PCC length incorrectly initialised")
 
-    @attr('capabilities')
     def test_cp2_reg_init_pcc_otype(self):
         self.assertRegisterEqual(self.MIPS.pcc.ctype, self.unsealed_otype, "CP2 PCC ctype incorrectly initialised")
 
-    @attr('capabilities')
     def test_cp2_reg_init_pcc_perms(self):
         self.assertRegisterAllPermissions(self.MIPS.pcc.perms, "CP2 PCC perms incorrectly initialised")
 
-    @attr('capabilities')
     def test_cp2_reg_init_pcc_unsealed(self):
         self.assertRegisterEqual(self.MIPS.pcc.s, 0, "CP2 PCC sealed incorrectly initialised")
 
-    @attr('capabilities')
     def test_cp2_reg_init_rest(self):
         '''Test that CP2 general-purpose register perms are correctly initialised'''
         for i in range(1, 32):
@@ -84,18 +79,15 @@ class test_cp2_reg_init(BaseBERITestCase):
             else:
                 self.assertNullCap(self.MIPS.cp2[i], "CP2 capability register $c" + str(i) + " incorrectly initialised")
 
-
-    @attr('capabilities')
     def test_cp2_reg_init_rest_unsealed(self):
         '''Test that CP2 general-purpose register unsealeds are correctly initialised'''
         for i in range(1, 26):
             self.assertRegisterEqual(self.MIPS.cp2[i].s, 0, "CP2 capability register sealed incorrectly initialised")
 
-    @attr('capabilities')
     def test_cp2_reg_init_unused_hwregs(self):
         '''Test that all cap hwregs that aren't defined in the spec are None'''
         for t in self.MIPS.threads.values():
-            for i in itertools.chain(range(2, 8), range(9, 22), range(24, 29)):
+            for i in itertools.chain(range(2, 8), range(9, 22), range(24, 28)):
                 self.assertIsNone(t.cp2_hwregs[i], msg="Hwreg " + str(i) + " should not exist")
 
     def _test_special_hwreg(self, name, number, is_null=False):
@@ -104,48 +96,42 @@ class test_cp2_reg_init(BaseBERITestCase):
             func(getattr(t, name), msg="$" + name + " incorrect")
             func(t.cp2_hwregs[number], msg="cap_hwr " + str(number) + " != $" + name + "?")
 
-    @attr('capabilities')
     def test_cp2_reg_init_ddc(self):
         '''Check that $ddc is correctly initialized to be the full addrespace'''
         for t in self.MIPS.threads.values():
             self.assertDefaultCap(t.ddc, msg="t.ddc should be a default cap")
             self.assertDefaultCap(t.cp2_hwregs[0], msg="cap_hwr 0 (ddc) should be a default cap")
 
-    @attr('capabilities')
     def test_cp2_reg_init_c0(self):
         '''Check that c0 is correctly initialized (either as $ddc or $cnull)'''
         for t in self.MIPS.threads.values():
             self.assertNullCap(t.cp2[0], msg="C0 should be the null register")
             self.assertNotEqual(t.cp2[0], t.cp2_hwregs[0], msg="C0 should not be a mirror of caphwr ddc")
 
-    @attr('capabilities')
     def test_cp2_reg_init_usertls_null(self):
         for t in self.MIPS.threads.values():
             self.assertNullCap(t.cp2_hwregs[1], msg="user tls reg should be null on reset")
 
-    @attr('capabilities')
     def test_cp2_reg_init_privtls_null(self):
         for t in self.MIPS.threads.values():
             self.assertNullCap(t.cp2_hwregs[8], msg="privileged tls reg should be null on reset")
 
-    @attr('capabilities')
     def test_cp2_reg_init_hwreg_kr1c_null(self):
         for t in self.MIPS.threads.values():
             self.assertNullCap(t.cp2_hwregs[22], msg="caphwr kr1c should be null on reset")
 
-    @attr('capabilities')
     def test_cp2_reg_init_hwreg_kr2c_null(self):
         for t in self.MIPS.threads.values():
             self.assertNullCap(t.cp2_hwregs[23], msg="caphwr kr1c should be null on reset")
 
-    @attr('capabilities')
+    def test_cp2_reg_init_error_epcc(self):
+        self._test_special_hwreg("error_epcc", 28)
+
     def test_cp2_reg_init_kcc(self):
         self._test_special_hwreg("kcc", 29)
 
-    @attr('capabilities')
     def test_cp2_reg_init_kdc(self):
         self._test_special_hwreg("kdc", 30, is_null=True)
 
-    @attr('capabilities')
     def test_cp2_reg_init_epcc(self):
         self._test_special_hwreg("epcc", 31)
