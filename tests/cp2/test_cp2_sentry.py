198f13273966830902589fc4a7b0d8509f5711c2
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2019-01-28 13:20:27 +0000

    Add a test for sentry capabilities (guarded by @attr("sentry_caps")

---
+++ tests/cp2/test_cp2_sentry.py
@@ -0,0 +1,119 @@
+#
+# Copyright (c) 2019 Alex Richardson
+# All rights reserved.
+#
+# This software was developed by SRI International and the University of
+# Cambridge Computer Laboratory under DARPA/AFRL contract FA8750-10-C-0237
+# ("CTSRD"), as part of the DARPA CRASH research programme.
+#
+# @BERI_LICENSE_HEADER_START@
+#
+# Licensed to BERI Open Systems C.I.C. (BERI) under one or more contributor
+# license agreements.  See the NOTICE file distributed with this work for
+# additional information regarding copyright ownership.  BERI licenses this
+# file to you under the BERI Hardware-Software License, Version 1.0 (the
+# "License"); you may not use this file except in compliance with the
+# License.  You may obtain a copy of the License at:
+#
+#   http://www.beri-open-systems.org/legal/license-1-0.txt
+#
+# Unless required by applicable law or agreed to in writing, Work distributed
+# under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+# CONDITIONS OF ANY KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations under the License.
+#
+# @BERI_LICENSE_HEADER_END@
+#
+
+from beritest_tools import BaseBERITestCase, attr, HexInt
+
+#
+# Test a ccall with addr check
+#
+@attr('capabilities')
+@attr('sentry_caps')
+class test_cp2_sentry(BaseBERITestCase):
+    EXPECTED_EXCEPTIONS = 8
+
+    def test_good_return_value(self):
+        assert self.MIPS.a0 == 42, "test did not run to completion"
+
+    def test_num_sandbox_calls(self):
+        assert self.MIPS.a7 == 6, "Should call the sandbox 6 times"
+
+    def test_attributes_sealed(self):
+        assert self.MIPS.a1 == 1, "cgetsealed $a1, $c1 should return true"
+
+    def test_attributes_perm(self):
+        assert self.MIPS.a2 == self.max_permissions, "cgetperm $a2, $c1 should return max exec perms"
+
+    def test_attributes_base(self):
+        assert self.MIPS.a3 == 0, "cgetbase $a3, $c1 wrong"
+
+    def test_attributes_offset(self):
+        assert self.MIPS.a4 == self.MIPS.c12.offset, "cgetoffset $a4, $c1 wrong"
+
+    def test_attributes_len(self):
+        assert self.MIPS.a5 == self.MIPS.c12.length, "cgetlen $a5, $c1 wrong"
+
+    def test_attributes_tag(self):
+        assert self.MIPS.a6 == 1, "cgettag $a6, $c1 wrong"
+
+    def test_attributes_type(self):
+        assert self.MIPS.t8 == HexInt(self.minus_one_as_u64 - 1), "cgettype $t3, $c1 should be -2"
+
+    def test_good_csealcode(self):
+        self.assertTrapInfoNoTrap(self.MIPS.s0, msg="CSealCode with normal $pcc should succeed")
+        assert self.MIPS.c1.s == 1, "Result of csealcode should be sealed!"
+
+    def test_bad_csealcode_permit_execute(self):
+        self.assertCp2Fault(self.MIPS.s1, cap_cause=self.MIPS.CapCause.Permit_Execute_Violation,
+                            cap_reg=4, msg="CSealCode without Permit_Execute should trap", trap_count=1)
+
+    def test_cseal_worked(self):
+        self.assertTrapInfoNoTrap(self.MIPS.s2, msg="CSeal should work")
+        assert self.MIPS.c7.s == 1, "$c7 should be sealed"
+
+    def test_bad_csealcode_sealed_source(self):
+        self.assertCp2Fault(self.MIPS.s3, cap_cause=self.MIPS.CapCause.Seal_Violation,
+                            cap_reg=7, msg="CSealCode without sealed source cap should trap", trap_count=2)
+
+    def test_sentry_immutable_cincoffset(self):
+        self.assertCp2Fault(self.MIPS.s4, cap_cause=self.MIPS.CapCause.Seal_Violation,
+                            cap_reg=1, msg="CIncOffset (nonzero offset) on sentry cap should trap", trap_count=3)
+
+    def test_sentry_immutable_candperm(self):
+        self.assertCp2Fault(self.MIPS.s5, cap_cause=self.MIPS.CapCause.Seal_Violation,
+                            cap_reg=1, msg="CAndPerm (nonzero offset) on sentry cap should trap", trap_count=4)
+
+    def test_sentry_immutable_cmove_ok(self):
+        self.assertTrapInfoNoTrap(self.MIPS.s6, msg="CMove on sentry should work")
+        assert self.MIPS.c19.s == 1, "$c21 should be sealed"
+
+    def test_sentry_immutable_cincoffset_0(self):
+        self.assertTrapInfoNoTrap(self.MIPS.s7, msg="CIncOffset 0 on sentry should work")
+        assert self.MIPS.c20.s == 1, "$c22 should be sealed"
+
+
+    def test_sentry_no_load_permitted(self):
+        self.assertCp2Fault(self.MIPS.t2, cap_cause=self.MIPS.CapCause.Seal_Violation,
+                            cap_reg=1, msg="Loading via sentry cap should trap", trap_count=8)
+        assert self.MIPS.t3 == 0xbad
+
+    def test_unsealed_call_perm_load(self):
+        self.assertIntCap(self.MIPS.c21, 0x1234, "Unsealed call should be able to load value")
+
+    def test_unsealed_call_no_load(self):
+        self.assertIntCap(self.MIPS.c22, 0xbad, "Unsealed call without perm_exe shoudl not load")
+
+    def test_sentry_cjalr_perm_load(self):
+        self.assertIntCap(self.MIPS.c23, 0x1234, "Sentry cjalr should be able to load value")
+
+    def test_unsealed_cjalr_noload(self):
+        self.assertIntCap(self.MIPS.c24, 0xbad, "Sentry cjalr without perm_exe shoudl not load")
+
+    def test_sentry_cjr_perm_load(self):
+        self.assertIntCap(self.MIPS.c25, 0x1234, "Sentry cjr should be able to load value")
+
+    def test_unsealed_cjr_noload(self):
+        self.assertIntCap(self.MIPS.c26, 0xbad, "Sentry cjr without perm_exe shoudl not load")
---
+++ tests/cp2/test_cp2_sentry.s
@@ -0,0 +1,134 @@
+#-
+# Copyright (c) 2019 Alex Richardson
+# All rights reserved.
+#
+# This software was developed by SRI International and the University of
+# Cambridge Computer Laboratory under DARPA/AFRL contract FA8750-10-C-0237
+# ("CTSRD"), as part of the DARPA CRASH research programme.
+#
+# @BERI_LICENSE_HEADER_START@
+#
+# Licensed to BERI Open Systems C.I.C. (BERI) under one or more contributor
+# license agreements.  See the NOTICE file distributed with this work for
+# additional information regarding copyright ownership.  BERI licenses this
+# file to you under the BERI Hardware-Software License, Version 1.0 (the
+# "License"); you may not use this file except in compliance with the
+# License.  You may obtain a copy of the License at:
+#
+#   http://www.beri-open-systems.org/legal/license-1-0.txt
+#
+# Unless required by applicable law or agreed to in writing, Work distributed
+# under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+# CONDITIONS OF ANY KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations under the License.
+#
+# @BERI_LICENSE_HEADER_END@
+#
+
+.include "macros.s"
+.include "macros_extra.s"
+
+
+.macro make_sentry out, in
+	cheri_2arg_insn 0x1d, \out, \in
+.endm
+
+BEGIN_TEST
+
+	dli $a7, 0	# number of sandbox calls
+
+	# create a code capability ($c7) and seal it ($c1) using the new CSealEntry instruction
+	cap_from_label	$c12, label=sandbox
+	# csealcode	$c1, $c1, $c12
+	check_instruction_traps $s0, make_sentry 1, 12		# should not trap
+
+	# This should trap since $c3 does not have permit_execute
+	dli	$t0, 0
+	candperm	$c4, $c12, $t0
+	cgetnull	$c5	# should still be null
+	# csealcode	$c1, $c5, $c4
+	check_instruction_traps $s1, make_sentry 5, 4		# trap #1
+
+	# check that we can't use CSealEntry with sealed caps
+	# create a seal cap for otyp 1234
+	dli		$t0, 0x1234
+	cgetdefault	$c16
+	csetoffset	$c16, $c16, $t0
+	check_instruction_traps  $s2, cseal	$c7, $c12, $c16		# should not trap
+	# sealed caps can't be sentries:
+	# csealcode	$c1, $c8, $c7
+	check_instruction_traps $s3, make_sentry 8, 7		# trap #2
+
+
+	# no check that we can't modify the sentry cap
+	check_instruction_traps $s4, cincoffset	$c18, $c1, 1	# trap #3
+	# TODO: should candperm be allowed?
+	check_instruction_traps $s5, candperm	$c18, $c1, $zero	# trap #4
+	# Check that we can move the value
+	check_instruction_traps $s6, cmove	$c19, $c1		# no trap
+	check_instruction_traps $s7, cincoffset	$c20, $c1, $zero	# no trap
+
+
+	# Now check properties of the sentry cap
+	cgetsealed $a1, $c1
+	cgetperm $a2, $c1
+	cgetbase $a3, $c1
+	cgetoffset $a4, $c1
+	cgetlen $a5, $c1
+	cgettag $a6, $c1
+	cgettype $t8, $c1
+
+	# Check that we can call the sandbox with the unsealed cap:
+	cjalr	$c12, $c17
+	CFromInt $c21, $t0	# Check that we loaded the "0x1234" with a normal call
+
+
+	# Check that we can call but not load the value if PERM_LOAD is missing
+	dli	$t0, ~CHERI_PERM_LOAD
+	candperm	$c13, $c12, $t0
+	cjalr	$c13, $c17
+	CFromInt $c22, $t0	# Check that we didn't load 0x1234
+
+
+	# Check that we can call the sandbox with the sentry cap:
+	cjalr	$c1, $c17
+	CFromInt $c23, $t0	# Check that we loaded the "0x1234" with a sentry cjalr
+
+	# test a sentry cap without permit_load
+	make_sentry	14, 13
+	cjalr	$c14, $c17
+	CFromInt $c24, $t0	# Check that didn't load "0x1234" with a no_load sentry cjalr
+
+	# Finally check that cjr works with sentry caps:
+	cap_from_label	$c17, label=return_result_1
+	cjr	$c1
+	nop
+return_result_1:
+	CFromInt $c25, $t0	# Check that we loaded the "0x1234" with a sentry cjr
+
+	cap_from_label	$c17, label=return_result_2
+	cjr	$c14
+	nop
+return_result_2:
+	CFromInt $c26, $t0	# Check that didn't load "0x1234" with a no_load sentry cjr
+
+
+	# finally check that we can't load via the sentry cap:
+	dli $t3, 0xbad
+	check_instruction_traps $t2, clb $t3, $zero, 0($c1) # trap #8
+
+	dla $a0, 42	# success
+return_fail:
+END_TEST
+
+.balign 1024	# add some padding
+.ent sandbox
+sandbox:
+		daddiu $a7, $a7, 1
+		dli	$t0, 0xbad	# ensure we don't accidentally keep the old value
+		cgetpcc $c18
+		cld	$t0, $zero, 16($c18)	# this should load 0x1234
+		cjr $c17
+		nop
+		.8byte 0x1234	# cgetpcc + 16
+.end sandbox
