f9697f3b2bfd21e76b47ad0da3bb9a9893f627c3
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2019-02-20 17:59:23 +0000

    Remove XFAIL from test_purecap_union_struct
    It has been fixed in clang

--- tests/purecap/test_purecap.py
+++ tests/purecap/test_purecap.py
@@ -58,8 +58,6 @@ def check_answer(test_name, test_file):
 
 
 def _get_xfail_reason(test_name):
-    if test_name == "test_purecap_union_struct":
-        return "This is failing due to a clang bug!"
     if test_name == "test_purecap_statcounters":
         # L3/SAIL don't implement the statcounters instructions
         if os.getenv("TEST_MACHINE", "").lower() in ("l3", "sail"):
