fece16f8b1c8c6b86be005d3defe537ed8ed90ef
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2019-04-17 10:43:08 -0400

    Align all return values of the fake operator new to sizeof(capability)
    This should fix the broken purecap C++ test and CHERI256

--- tests/purecap/fake_cxx_runtime.cpp
+++ tests/purecap/fake_cxx_runtime.cpp
@@ -1,12 +1,17 @@
 #include "cheri_c_test.h"
 
-static char _new_buffer[4096];
+/* Ensure that we get a capability aligned allocation */
+_Alignas(sizeof(void* __capability)) static char _new_buffer[4096];
 static unsigned _new_allocated = 0;
 
+// A very simple operator new implementation
 void* operator new(unsigned long size) {
 	void* result = _new_buffer + _new_allocated;
-	_new_allocated += size;
+	// Round the used size to capability size
+	_new_allocated += __builtin_align_up(size, sizeof(void* __capability));
 	assert(_new_allocated < sizeof(_new_buffer));
+	// Check that the result is correctly aligned
+	assert(__builtin_is_aligned(result, sizeof(void* __capability)));
 	return __builtin_cheri_bounds_set(result, size);
 }
 
