7db144937adbae5b4f6cca1dd3b670901f5eaae9
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2019-11-28 22:41:18 +0000

    Remove old code
    This is provided by cheri_init_globals.h now.

--- crt_init_globals.c
+++ crt_init_globals.c
@@ -1,5 +1,6 @@
 /*-
  * Copyright (c) 2014 Robert N. M. Watson
+ * Copyright (c) 2019 Alex Richardson
  * All rights reserved.
  *
  * This software was developed by SRI International and the University of
@@ -27,80 +28,12 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  */
-#ifdef __CHERI_CAPABILITY_TABLE__
 #include <cheri_init_globals.h>
+
 void
 crt_init_globals(void) {
 	cheri_init_globals();
 }
-#else
-// master doesn't install cheri_init_globals.h yet
-typedef long uint64_t;
-
-struct capreloc
-{
-    uint64_t capability_location;
-    uint64_t object;
-    uint64_t offset;
-    uint64_t size;
-    uint64_t permissions;
-};
-static const uint64_t function_reloc_flag = 1ULL<<63;
-static const uint64_t function_pointer_permissions =
-	~0 &
-	~__CHERI_CAP_PERMISSION_PERMIT_STORE_CAPABILITY__ &
-	~__CHERI_CAP_PERMISSION_PERMIT_STORE__;
-static const uint64_t global_pointer_permissions =
-	~0 & ~__CHERI_CAP_PERMISSION_PERMIT_EXECUTE__;
-
-
-__attribute__((weak))
-extern struct capreloc __start___cap_relocs;
-__attribute__((weak))
-extern struct capreloc __stop___cap_relocs;
-
-void
-crt_init_globals(void)
-{
-	struct capreloc *start_relocs;
-	struct capreloc *stop_relocs;
-#ifndef __CHERI_CAPABILITY_TABLE__
-
-	/* If we are not using the CHERI capability table we can just synthesize
-	 * the capabilities for these using the GOT and $ddc */
-	start_relocs = &__start___cap_relocs;
-	stop_relocs = &__stop___cap_relocs;
-#else
-	long start_addr, end_addr;
-	__asm__ (".option pic0\n\t"
-		 "dla %0, __start___cap_relocs\n\t"
-		 "dla %1, __stop___cap_relocs\n\t"
-		 :"=r"(start_addr), "=r"(end_addr));
-	long relocs_size = end_addr - start_addr;
-	void *ddc = __builtin_cheri_global_data_get();
-	start_relocs = __builtin_cheri_offset_set(__builtin_cheri_global_data_get(), start_addr);
-	start_relocs = __builtin_cheri_bounds_set(start_relocs, relocs_size);
-	stop_relocs = __builtin_cheri_offset_set(start_relocs, relocs_size);
-#endif
-	void *gdc = __builtin_cheri_global_data_get();
-	void *pcc = __builtin_cheri_program_counter_get();
-
-	gdc = __builtin_cheri_perms_and(gdc, global_pointer_permissions);
-	pcc = __builtin_cheri_perms_and(pcc, function_pointer_permissions);
-	for (struct capreloc *reloc = start_relocs; reloc < stop_relocs; reloc++) {
-		_Bool isFunction = (reloc->permissions & function_reloc_flag) == function_reloc_flag;
-		void **dest = __builtin_cheri_offset_set(gdc, reloc->capability_location);
-		void *base = isFunction ? pcc : gdc;
-		void *src = __builtin_cheri_offset_set(base, reloc->object);
-		if (!isFunction && (reloc->size != 0)) {
-			src = __builtin_cheri_bounds_set(src, reloc->size);
-		}
-		src = __builtin_cheri_offset_increment(src, reloc->offset);
-		*dest = src;
-	}
-}
-#endif
-
 
 // Also provide a simple purecap strlen function:
 __SIZE_TYPE__ strlen(const char* str)
